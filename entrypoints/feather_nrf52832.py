ea18655a210592f04af2c8070f980936b1e22907
Josh Klar <j@iv597.com>
2018-09-03 15:20:27 -0700

    On unhandled exceptions, blink the feather LED repeatedly

--- entrypoints/feather_nrf52832.py
+++ entrypoints/feather_nrf52832.py
@@ -1,4 +1,12 @@
+import sys
+
+from kmk.circuitpython.util import feather_red_led_flash
 from kmk_keyboard_user import main
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        sys.print_exception(e)
+        feather_red_led_flash(duration=10, rate=0.5)
+        sys.exit(1)
