04d30f1e42beddcfa6b2246164af8d406d8e81bb
Josh Klar <josh@klar.sh>
2018-10-05 15:39:50 -0700

    Make sure unix micropython refers to correct location

--- bin/micropython.sh
+++ bin/micropython.sh
@@ -1,6 +1,6 @@
-if [ ! -f vendor/micropython/ports/unix/micropython ]; then
+if [ ! -f build/micropython/ports/unix/micropython ]; then
 	echo "Run make micropython-build-unix"
 	exit 1
 fi
 
-vendor/micropython/ports/unix/micropython $@
+build/micropython/ports/unix/micropython $@
