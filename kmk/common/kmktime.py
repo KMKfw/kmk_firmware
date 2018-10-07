a23acc61318548c6a88cacc780558c141d0e4f69
Josh Klar <josh@klar.sh>
2018-10-07 00:44:04 -0700

    Update various internal key handling stuff to be CircuitPython-compatible

--- kmk/common/kmktime.py
+++ kmk/common/kmktime.py
@@ -1,13 +1,33 @@
+import math
 
 try:
-    import utime
+    import utime as time
+    USE_UTIME = True
 except ImportError:
-    pass
+    import time
+    USE_UTIME = False
+
+
+def sleep_ms(ms):
+    '''
+    Tries to sleep for a number of milliseconds in a cross-implementation
+    way. Will raise an ImportError if time is not available on the platform.
+    '''
+    if USE_UTIME:
+        return time.sleep_ms(ms)
+    else:
+        return time.sleep(ms / 1000)
 
 
 def ticks_ms():
-    return utime.ticks_ms()
+    if USE_UTIME:
+        return time.ticks_ms()
+    else:
+        return math.floor(time.monotonic() * 1000)
 
 
 def ticks_diff(new, old):
-    return utime.ticks_diff(new, old)
+    if USE_UTIME:
+        return time.ticks_diff(new, old)
+    else:
+        return new - old
--- kmk/common/util.py
+++ kmk/common/util.py
@@ -29,16 +29,3 @@ def reset_bootloader():
         import microcontroller
         microcontroller.on_next_reset(microcontroller.RunMode.BOOTLOADER)
         microcontroller.reset()
-
-
-def sleep_ms(ms):
-    '''
-    Tries to sleep for a number of milliseconds in a cross-implementation
-    way. Will raise an ImportError if time is not available on the platform.
-    '''
-    try:
-        import time
-        time.sleep_ms(ms)
-    except AttributeError:
-        import time
-        time.sleep(ms / 1000)
