faa61f7df102ff6a2ce5f3db487bc2503fa9d07c
Josh Klar <josh@klar.sh>
2018-12-29 07:27:44 -0800

    Remove debug leftovers causing failed lints

--- kmk/mcus/circuitpython_samd51.py
+++ kmk/mcus/circuitpython_samd51.py
@@ -1,8 +1,6 @@
 from kmk.firmware import Firmware as _Firmware
 from kmk.hid import CircuitPythonUSB_HID
 
-import kmk.keycodes
-
 
 class Firmware(_Firmware):
     hid_helper = CircuitPythonUSB_HID
