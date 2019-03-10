fa28b3afba8085fce5ac0bf2d6cff57cb23cb0c4
Kyle Brown <kdb424@gmail.com>
2019-03-09 18:28:10 -0800

    Luddite "fixes" for personal layout. Exposes a KMK limitation

--- user_keymaps/kdb424/luddite.py
+++ user_keymaps/kdb424/luddite.py
@@ -48,19 +48,25 @@ FN1 = 1
 
 keyboard.keymap = [
     [
-        [KC.GESC, KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.LBRC, KC.RBRC, KC.BSPC],
-        [KC.LEAD, KC.QUOT, KC.COMM, KC.DOT,  KC.P,    KC.Y,    KC.F,    KC.G,    KC.C,    KC.R,    KC.L,    KC.SLSH, KC.EQL,  KC.BSLS],
-        [KC.TAB,  KC.A,    KC.O,    KC.E,    KC.U,    KC.I,    KC.D,    KC.H,    KC.T,    KC.N,    KC.S,    KC.MINS,           KC.ENT],
-        [KC.LSFT, KC.SCLN, KC.Q,    KC.J,    KC.K,    KC.X,    KC.B,    KC.M,    KC.W,    KC.V,    KC.Z,                      KC.RSFT],
-        [KC.LCTL,  KC.LGUI, KC.MO(FN1),                       KC.SPC,                               KC.LEFT, KC.DOWN, KC.UP, KC.RIGHT],
+        [KC.GESC, KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7],
+        [KC.N8,   KC.N9,   KC.N0,   KC.LBRC, KC.RBRC, KC.BSPC, KC.LEAD, KC.QUOT],
+        [KC.COMM, KC.DOT,  KC.P,    KC.Y,    KC.F,    KC.G,    KC.C,    KC.R],
+        [KC.L,    KC.SLSH, KC.EQL,  KC.BSLS, KC.TAB,  KC.A,    KC.O,    KC.E],
+        [KC.U,    KC.I,    KC.D,    KC.H,    KC.T,    KC.N,    KC.S,    KC.MINS],
+        [KC.ENT,  KC.LSFT, KC.SCLN, KC.Q,    KC.J,    KC.K,    KC.X,    KC.B],
+        [KC.M,    KC.W,    KC.V,    KC.Z,    KC.RSFT, KC.LCTL, KC.LGUI, KC.MO(FN1)],
+        [KC.SPC, KC.LEFT, KC.DOWN, KC.UP, KC.RIGHT],
     ],
 
     [
-        [KC.GESC, KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,     KC.F11,   KC.F12,      KC.DEL],
-        [KC.RGB_TOG, _______, _______,   _______,   _______, _______, _______, _______, _______, _______, _______, _______, KC.VOLU, _______],
-        [_______,     _______, _______,   _______,   _______, _______, _______, _______, _______, _______, _______, KC.VOLD, _______],
-        [_______,      _______, _______,   _______,   _______, _______, _______, _______, _______, _______, _______, _______],
-        [KC.GRV, _______, _______,                           _______,                                      KC.LALT, _______, _______, _______],
+        [KC.GESC, KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.F7],
+        [KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  KC.DEL,  KC.RGB_TOG, _______],
+        [_______, _______, _______, _______, _______, _______, _______, _______],
+        [_______, _______, KC.VOLU, _______, _______, _______, _______, _______],
+        [_______, _______, _______, _______, _______, _______, _______, KC.VOLD],
+        [_______, _______, _______, _______, _______, _______, _______, _______],
+        [_______, _______, _______, _______, _______, KC.GRV,  _______, _______],
+        [_______, KC.LALT, _______, _______, _______],
     ],
 ]
 
