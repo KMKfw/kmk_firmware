1eaf0e2c79eea01a410224f84368db532e5b40bf
Josh Klar <josh@klar.sh>
2019-07-24 23:43:39 -0700

    flatten_dict is no longer used, remove it

--- kmk/util.py
+++ kmk/util.py
@@ -5,18 +5,6 @@ def get_wide_ordinal(char):
     return 0x10000 + (ord(char[0]) - 0xD800) * 0x400 + (ord(char[1]) - 0xDC00)
 
 
-def flatten_dict(d):
-    items = {}
-
-    for k, v in d.items():
-        if isinstance(v, dict):
-            items.update(flatten_dict(v))
-        else:
-            items[k] = v
-
-    return items
-
-
 def reset_keyboard():
     try:
         import machine
