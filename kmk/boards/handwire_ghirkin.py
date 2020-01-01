01451732c86761c3c3b8f5a8dca382f34149a00c
Kyle Brown <kdb424@gmail.com>
2020-01-01 13:40:19 -0800

    add handwire ghirkin

---
+++ kmk/boards/handwire_ghirkin.py
@@ -0,0 +1,25 @@
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
+from kmk.matrix import DiodeOrientation
+
+
+class KMKKeyboard(_KMKKeyboard):
+    col_pins = (
+        board.SDA,
+        board.A0,
+        board.A1,
+        board.A3,
+        board.A2,
+        board.A4
+    )
+    row_pins = (
+        board.MOSI,
+        board.MISO,
+        board.RX,
+        board.TX,
+        board.D2
+    )
+    diode_orientation = DiodeOrientation.COLUMNS
+    rgb_pixel_pin = None
+    rgb_num_pixels = 0
--- user_keymaps/kdb424/gherkin.py
+++ user_keymaps/kdb424/gherkin.py
@@ -1,49 +1,13 @@
-import board
-
+from kmk.boards.handwire_ghirkin import KMKKeyboard
 from kmk.consts import UnicodeMode
-from kmk.handlers.sequences import compile_unicode_string_sequences, send_string
+from kmk.handlers.sequences import compile_unicode_string_sequences
 from kmk.keys import KC
-from kmk.kmk_keyboard import KMKKeyboard
-from kmk.matrix import DiodeOrientation
-from kmk.types import AttrDict
 
 keyboard = KMKKeyboard()
 
-'''
-Converter/handwire:
-
-PB5: SCL
-PB4: SDA
-PE6: A0
-PD7: A1
-PC6: A2
-PD4: A3
-PD0: A4
-
-PB6: D2
-PB2: TX
-PB3: RX
-PB1: MI
-PF7: MO
-PF6: SCK
-PF5: A5
-
-Mosfet on B5 to control backlight
-'''
-
-keyboard.col_pins = (board.A4, board.A2, board.A3, board.A1, board.A0, board.SDA)
-keyboard.row_pins = (board.D2, board.TX, board.RX, board.MISO, board.MOSI)
-
-# Kyle is fucking stupid
-keyboard.col_pins = tuple(reversed(keyboard.col_pins))
-keyboard.row_pins = tuple(reversed(keyboard.row_pins))
-
-keyboard.diode_orientation = DiodeOrientation.COLUMNS
-
-
 # ------------------User level config variables ---------------------------------------
 keyboard.unicode_mode = UnicodeMode.LINUX
-keyboard.tap_time = 350
+keyboard.tap_time = 150
 keyboard.leader_timeout = 2000
 keyboard.debug_enabled = False
 
@@ -71,15 +35,31 @@ keyboard.leader_dictionary = {
     'yay': emoticons.YAY,
 }
 
-WPM = send_string('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum arcu vitae elementum curabitur vitae nunc sed. Facilisis sed odio morbi quis.')
 
+# Custom Keys
+_______ = KC.TRNS
+XXXXXXX = KC.NO
+
+LT1_X = KC.LT(1, KC.X)
+SHFT_SCLN = KC.MT(KC.SCLN, KC.LSFT)
+# BSPC = KC.TD(
+#        KC.SPC,
+#        KC.B,
+# )
 # ---------------------- Keymap ---------------------------------------------------------
 
 keyboard.keymap = [
+    # Layer 0
     [
-        KC.GESC,   KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,
-        KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.ESC,
-        KC.Z,    KC.X,    KC.C,    KC.V,    KC.BSPC, KC.SPC,  KC.B,    KC.N,    KC.M,    KC.ENT,
+        KC.QUOTE,  KC.COMMA,  KC.DOT,  KC.P,    KC.Y,    KC.F,    KC.G,    KC.C,    KC.R,    KC.L,
+        KC.A,      KC.O,      KC.E,    KC.U,    KC.I,    KC.D,    KC.H,    KC.T,    KC.N,    KC.S,
+        SHFT_SCLN, KC.Q,      KC.J,    KC.K,    LT1_X,   KC.SPC,  KC.M,    KC.W,    KC.V,   KC.Z,
+    ],
+    # Layer 1
+    [
+        KC.GESC,   KC.COMMA,  KC.DOT,  KC.P,    KC.Y,    KC.F,    KC.G,    KC.C,    KC.N0,   KC.BKSP,
+        KC.N1,     KC.N2,     KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.ENT,
+        SHFT_SCLN, KC.LGUI,   KC.J,    KC.K,    _______, KC.B,    KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT,
     ],
 ]
 
