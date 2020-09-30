797a615e5149f837ebcdbdeefed5a7752226704d
Kyle Brown <kdb424@gmail.com>
2020-09-30 12:30:05 -0700

    layout update

--- user_keymaps/kdb424/luddite.py
+++ user_keymaps/kdb424/luddite.py
@@ -33,6 +33,7 @@ HOME = KC.MT(KC.HOME, KC.LSFT)
 END = KC.MT(KC.END, KC.RSFT)
 LEFT_LAY = KC.LT(FN1, KC.LEFT)
 SHFT_INS = KC.LSFT(KC.INS)
+SPC = KC.LT(2, KC.SPC)
 
 
 # ---------------------- Keymap ---------------------------------------------------------
@@ -44,8 +45,8 @@ keyboard.keymap = [
         KC.GESC, KC.N1,   KC.N2,   KC.N3,  KC.N4,   KC.N5,   KC.N6,   KC.N7, KC.N8, KC.N9, KC.N0, KC.LBRC, KC.RBRC, KC.BSPC,
         KC.LEAD, KC.QUOT, KC.COMM, KC.DOT, KC.P,    KC.Y,    KC.F,    KC.G,  KC.C,  KC.R,  KC.L,  KC.SLSH, KC.EQL,  KC.BSLS,
         KC.TAB,  KC.A,    KC.O,    KC.E,   KC.U,    KC.I,    KC.D,    KC.H,  KC.T,  KC.N,  KC.S,  KC.MINS, KC.ENT,
-        HOME,    KC.SCLN, KC.Q,    KC.J,   KC.K,    KC.X,    KC.B,    KC.M,  KC.W,  KC.V,  KC.Z,  END,
-        KC.LCTL, KC.LGUI, KC.LALT,        KC.SPC,              LEFT_LAY, KC.DOWN, KC.UP,   KC.RIGHT,
+        KC.LSFT, KC.SCLN, KC.Q,    KC.J,   KC.K,    KC.X,    KC.B,    KC.M,  KC.W,  KC.V,  KC.Z,  KC.RSFT,
+        KC.LCTL, KC.LGUI, KC.LALT,           SPC,              KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT,
     ],
 
     # df
@@ -59,9 +60,9 @@ keyboard.keymap = [
 
     # fn
     [
-        KC.GESC,    KC.F1,      KC.F2,      KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.F7, KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  KC.DEL,
-        KC.RGB_TOG, KC.RGB_HUD, KC.RGB_HUI, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC.VOLU, SHFT_INS,
-        _______,    KC.RGB_SAD, KC.RGB_SAI, _______, _______, _______, _______, _______, _______, _______, _______, KC.VOLD, _______,
+        KC.GESC, KC.F1,      KC.F2,      KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.F7, KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  KC.DEL,
+        KC.RESET, KC.RGB_HUD, KC.RGB_HUI, _______, _______, _______, _______, _______, _______, KC.RGB_M_S, _______, _______, KC.VOLU, SHFT_INS,
+        _______,   KC.RGB_SAD, KC.RGB_SAI, _______, _______, _______, _______, _______, KC.RGB_TOG, _______, KC.RGB_M_P, KC.VOLD, _______,
         _______,    KC.RGB_VAD, KC.RGB_VAI, _______, _______, _______, _______, _______,    _______,    _______,    _______, _______,
         _______, _______,    _______,    _______, _______, _______, KC.DF(0),     KC.DF(1),
     ],
