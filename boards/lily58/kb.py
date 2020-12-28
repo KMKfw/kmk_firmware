33408dc228637d44b6689bcb979ad2b3a3234ae0
Kyle Brown <kdb424@gmail.com>
2020-12-27 22:09:52 -0800

    Lily58 works. Wired splits still don't

--- boards/lily58/kb.py
+++ boards/lily58/kb.py
@@ -7,14 +7,14 @@ from kmk.matrix import intify_coordinate as ic
 
 class KMKKeyboard(_KMKKeyboard):
     col_pins = (
-        board.P0_31,
-        board.P0_29,
         board.P0_02,
         board.P1_15,
         board.P1_13,
         board.P1_11,
+        board.P0_10,
+        board.P0_09,
     )
-    row_pins = (board.P0_22, board.P0_24, board.P1_00, board.P0_11)
+    row_pins = (board.P0_24, board.P1_00, board.P0_11, board.P1_04, board.P1_06)
     diode_orientation = DiodeOrientation.COLUMNS
     uart_pin = board.P0_08
     rgb_pixel_pin = board.P0_06
