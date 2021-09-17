c70966b0f6e3eaf7060e9825ed54ab95bcf43c06
Ryan Berwald <rberwald@gmail.com>
2021-09-17 15:15:35 -0400

    Fixed formatting again

--- kmk/extensions/oledDisplay_Example.py
+++ kmk/extensions/oledDisplay_Example.py
@@ -59,14 +59,14 @@ class bmpInfo:
     def createBitmap(self):
         '''The MIT License (MIT)
 
-Copyright (c) 2018 Scott Shawcroft for Adafruit Industries LLC
+        Copyright (c) 2018 Scott Shawcroft for Adafruit Industries LLC
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:'''
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:'''
         minimum_color_depth = 1
         while self.numColors > 2 ** minimum_color_depth:
             minimum_color_depth *= 2
