e3c1c86a0b3ec373115a2616100e382362e021fd
Yoichiro Tanaka <yoichiro6642@gmail.com>
2021-10-05 20:16:51 +0900

    Fix the linter errors.

--- boards/lunakey_pico/kb.py
+++ boards/lunakey_pico/kb.py
@@ -4,6 +4,7 @@ from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.matrix import DiodeOrientation
 from kmk.matrix import intify_coordinate as ic
 
+
 class KMKKeyboard(_KMKKeyboard):
     row_pins = (board.GP12, board.GP13, board.GP14, board.GP15)
     col_pins = (board.GP21, board.GP20, board.GP19, board.GP18, board.GP17, board.GP16)
