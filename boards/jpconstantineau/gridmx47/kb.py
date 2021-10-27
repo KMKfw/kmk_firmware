6aba450d25074e93e520d88720b7d43929113bc2
Pierre Constantineau <jpconstantineau@gmail.com>
2021-10-26 22:33:17 -0600

    some more linter fixes

--- boards/jpconstantineau/gridmx47/kb.py
+++ boards/jpconstantineau/gridmx47/kb.py
@@ -5,8 +5,9 @@ from kmk.matrix import DiodeOrientation
 
 
 class KMKKeyboard(_KMKKeyboard):
-    col_pins = (board.GP11, board.GP12, board.GP13, board.GP14, board.GP15, board.GP19, board.GP20,
-                board.GP21, board.GP22, board.GP26, board.GP27, board.GP28)
+    col_pins = (board.GP11, board.GP12, board.GP13, board.GP14,
+                board.GP15, board.GP19, board.GP20, board.GP21,
+                board.GP22, board.GP26, board.GP27, board.GP28)
     row_pins = (board.GP7, board.GP8, board.GP9, board.GP10)
     diode_orientation = DiodeOrientation.COL2ROW
     rgb_pixel_pin = board.GP6
