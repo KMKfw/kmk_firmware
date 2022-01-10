ef3d38f8ec9dcbe0f70bff67bed065016c7b641b
dherrada <dylan.herrada@adafruit.com>
2022-01-10 12:39:23 -0500

    Added navi10 led

--- boards/navi10/kb.py
+++ boards/navi10/kb.py
@@ -1,10 +1,14 @@
 import board
+import digitalio
 
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.matrix import DiodeOrientation
 
 
 class KMKKeyboard(_KMKKeyboard):
+    led = digitalio.DigitalInOut(board.D9)
+    led.direction = digitalio.Direction.OUTPUT
+    led.value = False
     row_pins = (board.D10, board.MOSI, board.MISO, board.D8)
     col_pins = (
         board.D4,
