17535a2f767548b7ed7948e681b76b9650c89615
René Giovanni Borella <rgb@mapster.dk>
2022-02-14 23:32:33 +0100

    Update kmk/modules/encoder.py
    Co-authored-by: Tonasz <27835465+Tonasz@users.noreply.github.com>

--- kmk/modules/encoder.py
+++ kmk/modules/encoder.py
@@ -229,9 +229,11 @@ class EncoderHandler(Module):
                         new_encoder = GPIOEncoder(*gpio_pins)
 
                     # In our case, we need to define keybord and encoder_id for callbacks
-                    new_encoder.on_move_do = lambda x: self.on_move_do(keyboard, idx, x)
-                    new_encoder.on_button_do = lambda x: self.on_button_do(
-                        keyboard, idx, x
+                    new_encoder.on_move_do = lambda x, bound_idx=idx: self.on_move_do(
+                        keyboard, bound_idx, x
+                    )
+                    new_encoder.on_button_do = lambda x, bound_idx=idx: self.on_button_do(
+                        keyboard, bound_idx, x
                     )
                     self.encoders.append(new_encoder)
                 except Exception as e:
