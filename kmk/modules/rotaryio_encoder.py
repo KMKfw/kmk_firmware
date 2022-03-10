a077d16a28eac032a312888565a4b9f7506ab0a9
Matteo Bellitti <bellitti@bu.edu>
2022-03-09 19:21:21 -0500

    fixed double quotes

--- kmk/modules/rotaryio_encoder.py
+++ kmk/modules/rotaryio_encoder.py
@@ -4,33 +4,26 @@ from kmk.modules import Module
 
 
 class RotaryIOEncoder(Module):
-    """This class enables the use of rotary encoders.
+    '''This class enables the use of rotary encoders.
     It is a pretty thin wrapper around rotaryio.IncrementalEncoder.
     WARNING: The two pins used for the encoder must be adjacent (e.g.
              GP16 and GP17 on a Pi pico)
 
     Does not support buttons, they should be part of the keyboard matrix
-    """
+    '''
 
     def __init__(self, encoders):
-        """self.encoders is a list of dictionaries, with keys
-        "encoder": rotaryio.IncrementalEncoder objext
-        "keymap": tuple of pairs (one per layer) with the keymap
-        "pin_a":
-        "pin_b":
-        "divisor":
-        """
 
         self.encoders = encoders
 
         for enc in self.encoders:
             # if you didn't give it explicitly you probably want divisor = 4
-            enc["divisor"] = enc.get("divisor", 4)
-            enc["encoder"] = rotaryio.IncrementalEncoder(
-                enc["pin_a"], enc["pin_b"], enc["divisor"]
+            enc['divisor'] = enc.get('divisor', 4)
+            enc['encoder'] = rotaryio.IncrementalEncoder(
+                enc['pin_a'], enc['pin_b'], enc['divisor']
             )
 
-            enc["old_position"] = enc["encoder"].position
+            enc['old_position'] = enc['encoder'].position
 
     def during_bootup(self, keyboard):
         return
@@ -42,14 +35,14 @@ class RotaryIOEncoder(Module):
 
         for enc in self.encoders:
 
-            change = enc["encoder"].position - enc["old_position"]
+            change = enc['encoder'].position - enc['old_position']
 
             if change != 0:
                 layer_id = keyboard.active_layers[0]
-                key = enc["keymap"][layer_id][1 if change > 0 else 0]
+                key = enc['keymap'][layer_id][1 if change > 0 else 0]
                 keyboard.tap_key(key)
 
-            enc["old_position"] = enc["encoder"].position
+            enc['old_position'] = enc['encoder'].position
 
         return keyboard
 
