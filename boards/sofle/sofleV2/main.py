ca773d063379a18673851df2d9003041f09de32a
ldsands <ldsands@outlook.com>
2022-04-05 18:18:04 -0500

    Update main.py
    I forgot to implement the default QMK keymap for Sofle

--- boards/sofle/sofleV2/main.py
+++ boards/sofle/sofleV2/main.py
@@ -23,33 +23,42 @@ keyboard.modules = [layers_ext, split]
 
 # Cleaner key names
 XXXXXXX = KC.NO
+UNDO = KC.LCTL(KC.Z)
+CUT = KC.LCTL(KC.X)
+COPY = KC.LCTL(KC.C)
+PASTE = KC.LCTL(KC.V)
+LSTRT = KC.LCTL(KC.HOME)
+LEND = KC.LCTL(KC.END)
+BACK = KC.LALT(KC.LEFT)
+NEXT = KC.LALT(KC.RGHT)
+LBSPC = KC.LCTL(KC.BSPC)
 LOWER = KC.MO(1)
 RAISE = KC.MO(2)
 
 keyboard.keymap = [
     [  #QWERTY
         #HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#ENCODER--#ENCODER--#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----
-        KC.ESC,   KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,                                            KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.BSPC,
-        KC.TAB,   KC.Q,     KC.W,     KC.E,     KC.R,     KC.T,                                             KC.Y,     KC.U,     KC.I,     KC.O,     KC.P,     KC.BSLS,
-        KC.LCTL,  KC.A,     KC.S,     KC.D,     KC.F,     KC.G,                                             KC.H,     KC.J,     KC.K,     KC.L,     KC.SCLN,  KC.QUOT,
+        KC.GRV,   KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,                                            KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.GRV,
+        KC.ESC,   KC.Q,     KC.W,     KC.E,     KC.R,     KC.T,                                             KC.Y,     KC.U,     KC.I,     KC.O,     KC.P,     KC.BSPC,
+        KC.TAB,   KC.A,     KC.S,     KC.D,     KC.F,     KC.G,                                             KC.H,     KC.J,     KC.K,     KC.L,     KC.SCLN,  KC.QUOT,
         KC.LSFT,  KC.Z,     KC.X,     KC.C,     KC.V,     KC.B,                                             KC.N,     KC.M,     KC.COMM,  KC.DOT,   KC.SLSH,  KC.RSFT,
-                            KC.LGUI,  KC.LALT,  KC.LCTL,  LOWER,    KC.ENT,   KC.MUTE,  KC.MPLY,  KC.SPC,   RAISE,    KC.RCTL,  KC.RALT,  KC.RCMD,
+                            KC.LGUI,  KC.LALT,  KC.LCTL,  LOWER,    KC.ENT,   KC.MUTE,  KC.MPLY,  KC.SPC,   RAISE,    KC.RCTL,  KC.RALT,  KC.RGUI,
     ],
     [  #LOWER
         #HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#ENCODER--#ENCODER--#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----
-        KC.ESC,   KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,                                            KC.F6,    KC.F7,    KC.F8,    KC.F9,    KC.F10,   KC.F11,
-        XXXXXXX,  KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,                                            KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.F12,
-        KC.LCTL,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                          KC.LEFT,  KC.DOWN,  KC.UP,    KC.RIGHT, XXXXXXX,  XXXXXXX,
-        KC.LSFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                          XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                            KC.LGUI,  KC.LALT,  KC.LCTL,  LOWER,    KC.ENT,   XXXXXXX,  XXXXXXX,  KC.SPC,   RAISE,    KC.RCTL,  KC.RALT,  KC.RCMD,
+        XXXXXXX,  KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,                                            KC.F6,    KC.F7,    KC.F8,    KC.F9,    KC.F10,   KC.F11,
+        KC.GRV,   KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,                                            KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.F12,
+        XXXXXXX,  KC.EXLM,  KC.AT,    KC.HASH,  KC.DLR,   KC.PERC,                                          KC.CIRC,  KC.AMPR,  KC.ASTR,  KC.LPRN,  KC.RPRN,  KC.PIPE,
+        XXXXXXX,  KC.EQL,   KC.MINS,  KC.PLUS,  KC.LCBR,  KC.RCBR,                                          KC.LBRC,  KC.RBRC,  KC.SCLN,  KC.COLN,  KC.BSLS,  XXXXXXX,
+                            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
     ],
     [  #RAISE
         #HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#ENCODER--#ENCODER--#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----#HERE-----
-        KC.ESC,   KC.EXLM,  KC.AT,    KC.HASH,  KC.DLR,   KC.PERC,                                          XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.BSPC,
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                          KC.CIRC,  KC.AMPR,  KC.ASTR,  KC.LPRN,  KC.RPRN,  KC.BSLS,
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                          KC.MINS,  KC.EQL,   KC.LBRC,  KC.RBRC,  KC.PIPE,  XXXXXXX,
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                          KC.UNDS,  KC.PLUS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                            KC.LGUI,  KC.LALT,  KC.LCTL,  LOWER,    KC.ENT,   KC.MUTE,  XXXXXXX,  KC.SPC,   RAISE,    KC.RCTL,  KC.RALT,  KC.RCMD,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                          XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        XXXXXXX,  KC.INS,   KC.PSCR,  KC.APP,   XXXXXXX,  XXXXXXX,                                          KC.PGUP,  BACK,     KC.UP,    NEXT,     LBSPC,    KC.BSPC,
+        XXXXXXX,  KC.LALT,  KC.LCTL,  KC.LSFT,  XXXXXXX,  KC.CAPS,                                          KC.PGDN,  KC.LEFT,  KC.DOWN,  KC.RGHT,  KC.DEL,   KC.BSPC,
+        XXXXXXX,  UNDO,     CUT,      COPY,     PASTE,    XXXXXXX,                                          XXXXXXX,  LSTRT,    XXXXXXX,  LEND,     XXXXXXX,  XXXXXXX,
+                            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
     ]
 ]
 
