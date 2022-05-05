c42a1fa032e0dc1ce7e8b91cc6d5a4c1f59861af
John Morrison <john@morrison.ec>
2022-05-05 07:55:37 +0100

    Conform class name case conventions

--- docs/extension_stringy_keymaps.md
+++ docs/extension_stringy_keymaps.md
@@ -5,7 +5,7 @@ Enables referring to keys by `'NAME'` rather than `KC.NAME`.
 For example:
 
 ```python
-from kmk.extensions.stringy_keymaps import Stringy_keymaps
+from kmk.extensions.stringy_keymaps import StringyKeymaps
 
 # Normal
 # keyboard.keymap = [[ KC.A, KC.B, KC.RESET ]]
@@ -16,13 +16,13 @@ from kmk.extensions.stringy_keymaps import Stringy_keymaps
 # String names
 keyboard.keymap = [[ 'A' , 'B', 'RESET' ]]
 
-stringy_keymaps = Stringy_keymaps()
+stringyKeymaps = StringyKeymaps()
 
 # Enabling debug will show each replacement or failure.
 # This is recommended during the initial development of a keyboard.
-# stringy_keymaps.debug_enable = True
+# stringyKeymaps.debug_enable = True
 
-keyboard.extensions.append(stringy_keymaps)
+keyboard.extensions.append(stringyKeymaps)
 ```
 
 It should be noted that these are **not** ASCII. The string is **not** what
--- tests/test_kmk_extension_stringy_keymaps.py
+++ tests/test_kmk_extension_stringy_keymaps.py
@@ -1,6 +1,6 @@
 import unittest
 
-from kmk.extensions.stringy_keymaps import Stringy_keymaps
+from kmk.extensions.stringy_keymaps import StringyKeymaps
 from kmk.keys import KC
 from tests.keyboard_test import KeyboardTest
 
@@ -8,14 +8,14 @@ from tests.keyboard_test import KeyboardTest
 class Test_extension_stringy_keymaps(unittest.TestCase):
     def test_basic_kmk_keyboard_replace_string_primary_name(self):
         keyboard = KeyboardTest(
-            [], [['1', '2', '3', '4']], extensions={Stringy_keymaps()}
+            [], [['1', '2', '3', '4']], extensions={StringyKeymaps()}
         )
 
         keyboard.test('Simple key press', [(0, True), (0, False)], [{KC.N1}, {}])
 
     def test_basic_kmk_keyboard_replace_string_secondary_name(self):
         keyboard = KeyboardTest(
-            [], [['N1', 'N2', 'N3', 'N4']], extensions={Stringy_keymaps()}
+            [], [['N1', 'N2', 'N3', 'N4']], extensions={StringyKeymaps()}
         )
 
         keyboard.test('Simple key press', [(0, True), (0, False)], [{KC.N1}, {}])
