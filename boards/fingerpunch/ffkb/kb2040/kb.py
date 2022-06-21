d7bfb917f2f9ca1d5333ca16f96ae3495f8d57ed
Luke D Russell <lukedrussell+git@outlook.com>
2022-06-21 17:18:33 +1000

    More Flake8 fixes, and keymap updates.

--- boards/fingerpunch/ffkb/kb2040/kb.py
+++ boards/fingerpunch/ffkb/kb2040/kb.py
@@ -29,6 +29,7 @@ class KMKKeyboard(_KMKKeyboard):
     rgb_num_pixels = 42
     i2c = board.I2C
 
+# flake8: noqa
     coord_mapping = [
             ic(0, 0), ic(0, 1), ic(0, 2), ic(0, 3), ic(0, 4), ic(0, 5),           ic(0, 6), ic(0, 7), ic(4, 3), ic(3, 4), ic(4, 5), ic(3, 7),
             ic(1, 0), ic(1, 1), ic(1, 2), ic(1, 3), ic(1, 4), ic(1, 5), ic(4, 1), ic(1, 6), ic(1, 7), ic(3, 2), ic(4, 4), ic(3, 5), ic(4, 7),
