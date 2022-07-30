b4c26c457b6fa01c8a2ef5c41eb054c3c14540ea
Leon Anavi <leon.anavi@konsulko.com>
2022-07-30 13:52:34 +0300

    boards/anavi/knobs3: Simplify the source code
    Optimize and simplify the source code by using the full potential
    of class EncoderHandler.
    
    Signed-off-by: Leon Anavi <leon.anavi@konsulko.com>

--- boards/anavi/knobs3/code.py
+++ boards/anavi/knobs3/code.py
@@ -1,23 +1,31 @@
 import board
 
-from anaviknob import AnaviKnob
-
+from kmk.extensions.media_keys import MediaKeys
 from kmk.extensions.RGB import RGB, AnimationModes
 from kmk.keys import KC
+from kmk.kmk_keyboard import KMKKeyboard
 from kmk.modules.encoder import EncoderHandler
+from kmk.scanners.keypad import KeysScanner
 
+knob = KMKKeyboard()
+knob.matrix = KeysScanner([])
+
+media_keys = MediaKeys()
+knob.extensions.append(media_keys)
+
+# Rotary encoders that also acts as keys
 encoder_handler = EncoderHandler()
 encoder_handler.pins = (
-    (board.D1, board.D2, None, False),
-    (board.D9, board.D10, None, False),
-    (board.D7, board.D8, None, False),
+    (board.D1, board.D2, board.D0),
+    (board.D9, board.D10, board.D3),
+    (board.D7, board.D8, board.D6),
 )
 encoder_handler.map = (
-    ((KC.VOLD, KC.VOLU), (KC.UP, KC.DOWN), (KC.RIGHT, KC.LEFT)),  # base layer
+    ((KC.VOLD, KC.VOLU, KC.MUTE), (KC.UP, KC.DOWN, KC.A), (KC.RIGHT, KC.LEFT, KC.B)),
 )
-
-knob = AnaviKnob()
 knob.modules.append(encoder_handler)
+
+print('ANAVI Knobs 3')
 
 rgb_ext = RGB(
     pixel_pin=board.NEOPIXEL,
@@ -28,7 +36,7 @@ rgb_ext = RGB(
 )
 knob.extensions.append(rgb_ext)
 
-knob.keymap = [[KC.MUTE, KC.A, KC.B]]
+knob.keymap = [[KC.MUTE]]
 
 if __name__ == '__main__':
     knob.go()
