ac75cc577e1dc282ab8e3854888ff2d933b321c7
Matthew Hendrix <hendrix04@gmail.com>
2022-08-03 09:10:33 -0400

    Updating keymap to use stringy and changing format

--- boards/Pico87/main.py
+++ boards/Pico87/main.py
@@ -1,10 +1,10 @@
 import board
 
 from kb import KMKKeyboard
-
-from kmk.extensions.lock_status import LockStatus
 from kmk.keys import KC
 from kmk.modules.layers import Layers
+from kmk.extensions.lock_status import LockStatus
+from kmk.extensions.stringy_keymaps import StringyKeymaps
 
 Pico87 = KMKKeyboard()
 
@@ -12,6 +12,7 @@ Pico87.modules.append(Layers())
 
 locks = LockStatus()
 Pico87.extensions.append(locks)
+Pico87.extensions.append(StringyKeymaps())
 
 
 def toggle_caps_led(key, keyboard, *args):
@@ -32,31 +33,30 @@ def led_1_off():
 # led_1_off()
 
 KC.CAPS.after_release_handler(toggle_caps_led)
-MOLAYER = KC.MO(1)
+MOLYR = KC.MO(1)
 # MOLAYER.after_press_handler(led_1_on)
 # MOLAYER.after_release_handler(led_1_off)
 
 # Make this for better looking formatting...
-_______ = KC.NO
+______ = 'NO'
 
-Pico87.keymap = [
-    [  # Layer 0 QWERTY
-         KC.ESC, _______,   KC.F1,   KC.F2,   KC.F3,   KC.F4, _______,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,  KC.F10,  KC.F11,  KC.F12, KC.PSCR, KC.SLCK, KC.PAUS,
-         KC.GRV,   KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0, KC.MINS,  KC.EQL, _______, KC.BSPC,  KC.INS, KC.HOME, KC.PGUP,
-         KC.TAB, _______,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P, KC.LBRC, KC.RBRC, KC.BSLS,  KC.DEL,  KC.END, KC.PGDN,
-        KC.CAPS, _______,    KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,  KC.ENT, _______, _______, _______, _______,
-        _______, KC.LSFT,    KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, _______, KC.RSFT, _______, _______,   KC.UP, _______,
-        KC.LCTL, KC.LGUI, _______, KC.LALT, _______, _______,  KC.SPC, _______, _______, _______, KC.RALT, KC.RGUI, _______, MOLAYER, KC.RCTL, KC.LEFT, KC.DOWN, KC.RGHT,
-    ],
-    [  # Layer 1
-         KC.ESC, _______,   KC.F1,   KC.F2,   KC.F3,   KC.F4, _______,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,  KC.F10,  KC.F11,  KC.F12, KC.PSCR, KC.SLCK, KC.PAUS,
-         KC.GRV,   KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0, KC.MINS,  KC.EQL, _______, KC.BSPC,  KC.INS, KC.HOME, KC.PGUP,
-         KC.TAB, _______,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P, KC.LBRC, KC.RBRC, KC.BSLS,  KC.DEL,  KC.END, KC.PGDN,
-        KC.CAPS, _______,    KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,  KC.ENT, _______, _______, _______, _______,
-        _______, KC.LSFT,    KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, _______, KC.RSFT, _______, _______,   KC.UP, _______,
-        KC.LCTL, KC.LGUI, _______, KC.LALT, _______, _______,  KC.SPC, _______, _______, _______, KC.RALT, KC.RGUI, _______, MOLAYER, KC.RCTL, KC.LEFT, KC.DOWN, KC.RGHT,
-    ],
-]
+Pico87.keymap = [[
+  # Layer 0 QWERTY
+    'ESC', ______,   'F1',   'F2',   'F3',   'F4', ______,   'F5',   'F6',   'F7',   'F8',   'F9',  'F10',  'F11',  'F12', 'PSCR', 'SLCK', 'PAUS',
+    'GRV',   'N1',   'N2',   'N3',   'N4',   'N5',   'N6',   'N7',   'N8',   'N9',   'N0', 'MINS',  'EQL', ______, 'BSPC',  'INS', 'HOME', 'PGUP',
+    'TAB', ______,    'Q',    'W',    'E',    'R',    'T',    'Y',    'U',    'I',    'O',    'P', 'LBRC', 'RBRC', 'BSLS',  'DEL',  'END', 'PGDN',
+   'CAPS', ______,    'A',    'S',    'D',    'F',    'G',    'H',    'J',    'K',    'L', 'SCLN', 'QUOT',  'ENT', ______, ______, ______, ______,
+   ______, 'LSFT',    'Z',    'X',    'C',    'V',    'B',    'N',    'M', 'COMM',  'DOT', 'SLSH', ______, 'RSFT', ______, ______,   'UP', ______,
+   'LCTL', 'LGUI', ______, 'LALT', ______, ______,  'SPC', ______, ______, ______, 'RALT', 'RGUI', ______,  MOLYR, 'RCTL', 'LEFT', 'DOWN', 'RGHT',
+], [
+  # Layer 1
+    'ESC', ______,   'F1',   'F2',   'F3',   'F4', ______,   'F5',   'F6',   'F7',   'F8',   'F9',  'F10',  'F11',  'F12', 'PSCR', 'SLCK', 'PAUS',
+    'GRV',   'N1',   'N2',   'N3',   'N4',   'N5',   'N6',   'N7',   'N8',   'N9',   'N0', 'MINS',  'EQL', ______, 'BSPC',  'INS', 'HOME', 'PGUP',
+    'TAB', ______,    'Q',    'W',    'E',    'R',    'T',    'Y',    'U',    'I',    'O',    'P', 'LBRC', 'RBRC', 'BSLS',  'DEL',  'END', 'PGDN',
+   'CAPS', ______,    'A',    'S',    'D',    'F',    'G',    'H',    'J',    'K',    'L', 'SCLN', 'QUOT',  'ENT', ______, ______, ______, ______,
+   ______, 'LSFT',    'Z',    'X',    'C',    'V',    'B',    'N',    'M', 'COMM',  'DOT', 'SLSH', ______, 'RSFT', ______, ______,   'UP', ______,
+   'LCTL', 'LGUI', ______, 'LALT', ______, ______,  'SPC', ______, ______, ______, 'RALT', 'RGUI', ______,  MOLYR, 'RCTL', 'LEFT', 'DOWN', 'RGHT',
+]]
 
 if __name__ == '__main__':
     Pico87.go()
