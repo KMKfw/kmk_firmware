c2a286f9e1e67fcfa122637d16a9edab4f8ee883
Kyle Brown <kdb424@gmail.com>
2022-10-01 17:21:40 -0700

    Quickpin initial support

---
+++ kmk/quickpin/pro_micro/sparkfun_promicro_rp2040.py
@@ -0,0 +1,28 @@
+import board
+
+pinout = [
+    board.TX,
+    board.RX,
+    None,  # GND
+    None,  # GND
+    board.D2,
+    board.D3,
+    board.D4,
+    board.D5,
+    board.D6,
+    board.D7,
+    board.D8,
+    board.D9,
+    board.D21,
+    board.MOSI,
+    board.MISO,
+    board.SCK,
+    board.D26,
+    board.D27,
+    board.D28,
+    board.D29,
+    None,  # 3.3v
+    None,  # RST
+    None,  # GND
+    None,  # RAW
+]
