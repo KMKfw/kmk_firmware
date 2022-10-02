9431ce5ec1339771c1abc516aef1effaeb7800de
Kyle Brown <kdb424@gmail.com>
2022-10-01 17:21:40 -0700

    Quickpin initial support

--- boards/boardsource/3x4/kb.py
+++ boards/boardsource/3x4/kb.py
@@ -1,12 +1,12 @@
 import board
 
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
+from kmk.quickpin.pro_micro.boardsource_blok import pinout as pins
 from kmk.scanners import DiodeOrientation
 
 
 class KMKKeyboard(_KMKKeyboard):
-    row_pins = (board.P1_15, board.P0_02, board.P0_29)
-    col_pins = (board.P0_09, board.P0_10, board.P1_11, board.P1_13)
+    row_pins = (pins[16], pins[17], pins[18])
+    col_pins = (pins[12], pins[13], pins[14], pins[15])
     diode_orientation = DiodeOrientation.COLUMNS
     i2c = board.I2C
-    powersave_pin = board.P0_13
---
+++ kmk/quickpin/pro_micro/nice_nano.py
@@ -0,0 +1,28 @@
+import board
+
+pinout = [
+    board.TX,
+    board.RX,
+    None,  # GND
+    None,  # GND
+    board.SDA,
+    board.SCL,
+    board.P0_22,
+    board.P0_24,
+    board.P1_00,
+    board.P0_11,
+    board.P1_04,
+    board.P1_06,
+    board.P0_09,
+    board.P0_10,
+    board.P1_11,
+    board.P1_13,
+    board.P1_15,
+    board.P0_02,
+    board.P0_29,
+    board.P0_31,
+    None,  # 3.3v
+    None,  # RST
+    None,  # GND
+    None,  # Battery+
+]
