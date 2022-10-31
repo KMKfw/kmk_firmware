08dfa940ff45090726124a7be9b054f17efc3a63
Alan <alan.bloomer@gmail.com>
2022-10-31 19:26:18 +0000

    Fix links in README, keymap in main.py

--- boards/ferris_sweep/main.py
+++ boards/ferris_sweep/main.py
@@ -8,16 +8,23 @@ from kb import KMKKeyboard
 from kmk.keys import KC
 from kmk.modules.layers import Layers
 from kmk.modules.modtap import ModTap
-from kmk.modules.split import Split
 from kmk.modules.mouse_keys import MouseKeys
-from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.split import Split, SplitSide
 
 keyboard = KMKKeyboard()
 
+# Adding extensions
+# rgb = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=27, val_limit=100, hue_default=190, sat_default=100, val_default=5)
+
+# TODO Comment one of these on each side
+# split_side = SplitSide.LEFT
+split_side = SplitSide.RIGHT
 split = Split(
-    split_flip=True,
-    # Ferris Sweep communicates on RX position of Pro Micro pinout, requires PIO
+    # split_flip=True,
     data_pin=board.D1,
+    split_side=split_side,
+    split_target_left=False,
+    # Using the default wasn't working, try pio
     use_pio=True,
     uart_flip=True,
 )
@@ -25,20 +32,24 @@ split = Split(
 layers_ext = Layers()
 mod_tap = ModTap()
 mouse_key = MouseKeys()
-media_key = MediaKeys()
-keyboard.modules = [layers_ext, mod_tap, split, mouse_key, media_key]
+
+
+keyboard.modules = [layers_ext, split, mod_tap, mouse_key]
 
 # Cleaner key names
 _______ = KC.TRNS
 XXXXXXX = KC.NO
 
+
 # Mod-taps
 A_SFT = KC.MT(KC.A, KC.LSFT)
 SCLN_SFT = KC.MT(KC.SCLN, KC.LSFT)
-X_CTL = KC.MT(KC.A, KC.LCTRL)
-C_ALT = KC.MT(KC.A, KC.LALT)
+X_CTL = KC.MT(KC.X, KC.LCTRL)
+C_ALT = KC.MT(KC.C, KC.LALT)
 COM_ALT = KC.MT(KC.COMM, KC.LALT)
 DOT_CTL = KC.MT(KC.DOT, KC.LCTRL)
+CTL_ALT = KC.LCTRL(KC.LALT)
+
 
 # Layer tap for other home row keys
 S_L5 = KC.LT(5, KC.S)
@@ -49,16 +60,14 @@ K_L2 = KC.LT(2, KC.K)
 L_L6 = KC.LT(6, KC.L)
 SPC_L7 = KC.LT(7, KC.SPC)
 
-# Mods
-CTL_ALT = KC.LCTL(KC.LALT)
-
 # fmt: off
+
 keyboard.keymap = [
     [  # QWERTY
-     KC.Q,  KC.W,  KC.E, KC.R,    KC.T,   KC.Y, KC.U,    KC.I,    KC.O,     KC.P,
-    A_SFT,  S_L5,  D_L1, F_L3,    KC.G,   KC.H, J_L4,    K_L2,    L_L6, SCLN_SFT,
+    KC.Q,KC.W,  KC.E, KC.R,KC.T,  KC.Y, KC.U,KC.I,KC.O,KC.P,
+   A_SFT,S_L5,  D_L1, F_L3,KC.G,  KC.H, J_L4,K_L2,L_L6,SCLN_SFT,
      KC.Z, X_CTL, C_ALT, KC.V,    KC.B,   KC.N, KC.M, COM_ALT, DOT_CTL,  KC.SLSH,
-                         KC.0, KC.BSPC, SPC_L7, KC.1,
+        KC.LGUI,        KC.BSPC,SPC_L7,        KC.ENT,
     ],
     [  # MOUSE
     _______,   _______, _______,   _______, _______, _______, KC.MB_LMB, KC.MW_UP, KC.MB_LMB,  _______,
@@ -82,32 +91,26 @@ keyboard.keymap = [
     _______, KC.COLN, KC.LABK, KC.RABK, KC.SCLN, _______, _______, _______, _______, _______,
     KC.LCBR, KC.RCBR, KC.LPRN, KC.RPRN,   KC.AT, _______, _______,  KC.EQL, KC.PLUS, KC.PERC,
     _______, KC.EXLM, KC.LBRC, KC.RBRC, _______, _______, _______, _______, _______, _______,
-                               KC.VOLD, _______, _______, KC.VOLU,
+                               _______, _______, _______, _______,
     ],
-    [  # FUNCTION
+    [  # 5 FUNCTION
     _______, _______, _______, _______, _______, _______, KC.F7, KC.F8, KC.F9, KC.F10,
     _______, _______, _______, _______, _______, _______, KC.F4, KC.F5, KC.F6, KC.F11,
     _______, _______, _______, _______, _______, _______, KC.F1, KC.F2, KC.F3, KC.F12,
                                _______, _______, _______, _______,
     ],
-    [  # NUMBERS
+    [  # 6 NUMBERS
     KC.SLSH,   KC.N7,   KC.N8,   KC.N9, KC.PLUS, _______, _______, _______, _______, _______,
       KC.N0,   KC.N1,   KC.N2,   KC.N3, KC.MINS, _______, _______, _______, _______, _______,
     KC.ASTR,   KC.N4,   KC.N5,   KC.N6,  KC.EQL, _______, _______, _______, _______, _______,
                                _______, _______, _______, _______,
     ],
-    [  # ALWAYS AVAILABLE
+    [  # 7 ALWAYS AVAILABLE
     _______, _______, KC.COLN,  KC.ESC, _______, _______, _______, _______, _______,  KC.DEL,
     _______, KC.PERC, KC.SLSH,  KC.ENT, _______, KC.DF(1), KC.LGUI, _______, _______, _______,
     _______, _______, _______, KC.PERC, _______, KC.DF(0), KC.RALT, KC.RCTL, _______, KC.RESET,
                                _______,  KC.TAB, _______, _______,
     ],
-    # [  # BLANK
-    # _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    # _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    # _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    #                            _______, _______, _______, _______,
-    # ],
 ]
 
 if __name__ == "__main__":
