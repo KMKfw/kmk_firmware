99542b0493f71f2b87bcc70eb6697eaa592118be
Dave Bendit <David@ibendit.com>
2022-11-21 20:53:35 +0000

    fix: Linting

--- tests/test_capsword.py
+++ tests/test_capsword.py
@@ -4,7 +4,8 @@ from kmk.keys import KC
 from kmk.modules.capsword import CapsWord
 from tests.keyboard_test import KeyboardTest
 
-#TODO: Add tests for custom and default ignored keys, custom and default timeouts
+# TODO: Add tests for custom and default ignored keys, custom and default timeouts
+
 
 class TestCapsWord(unittest.TestCase):
     def setUp(self):
@@ -19,8 +20,44 @@ class TestCapsWord(unittest.TestCase):
     def test_capsword(self):
         self.kb.test(
             'CapsWord',
-            [(1, True), (1, False), (0, True), (0, False), (1, True), (1, False), (2, True), (2, False), (3, True), (3, False), (4, True), (4, False), (1, True), (1, False), (5, True), (5, False), (1, True), (1, False)],
-            [{KC.A}, {}, {KC.LSFT, KC.A}, {}, {KC.LSFT, KC.Z}, {}, {KC.N1}, {}, {KC.N0}, {}, {KC.LSFT, KC.A}, {}, {KC.SPC}, {}, {KC.A}, {}],
+            [
+                (1, True),
+                (1, False),
+                (0, True),
+                (0, False),
+                (1, True),
+                (1, False),
+                (2, True),
+                (2, False),
+                (3, True),
+                (3, False),
+                (4, True),
+                (4, False),
+                (1, True),
+                (1, False),
+                (5, True),
+                (5, False),
+                (1, True),
+                (1, False),
+            ],
+            [
+                {KC.A},
+                {},
+                {KC.LSFT, KC.A},
+                {},
+                {KC.LSFT, KC.Z},
+                {},
+                {KC.N1},
+                {},
+                {KC.N0},
+                {},
+                {KC.LSFT, KC.A},
+                {},
+                {KC.SPC},
+                {},
+                {KC.A},
+                {},
+            ],
         )
 
 
