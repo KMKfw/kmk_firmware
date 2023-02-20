d5106dd5e2aadb148996f5d3d90afe117a9d863e
cole <smithcole@protonmail.com>
2023-02-19 18:44:10 -0800

    editing build script

--- docker_mpy_build.sh
+++ docker_mpy_build.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 echo "building mpy"
-ls compiled
+echo $(ls compiled)
 
 find kmk/ -name "*.py" -exec sh -c 'mkdir -p compiled/8/$(dirname {}) &&\
  ./mpy-cross-8 -O2 {} -o compiled/8/$(dirname {})/$(basename -s .py {}).mpy' \;
@@ -9,5 +9,6 @@ find kmk/ -name "*.py" -exec sh -c 'mkdir -p compiled/8/$(dirname {}) &&\
 find kmk/ -name "*.py" -exec sh -c 'mkdir -p compiled/7/$(dirname {}) &&\
  ./mpy-cross-7 -O2 {} -o compiled/7/$(dirname {})/$(basename -s .py {}).mpy' \;
 
-ls compiled/
+echo $(ls compiled)
+
 echo "done building"
