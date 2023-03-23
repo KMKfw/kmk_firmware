e6d9d4a3edc37cf37d36ff79b56ced50a1d6c078
doesntfazer <39163867+doesntfazer@users.noreply.github.com>
2023-03-23 17:20:38 -0400

    Refactored Layers module.

--- kmk/modules/layers.py
+++ kmk/modules/layers.py
@@ -1,136 +1,113 @@
-'''One layer isn't enough. Adds keys to get to more of them'''
 from kmk.keys import KC, make_argumented_key
 from kmk.modules.holdtap import HoldTap, HoldTapKeyMeta
 from kmk.utils import Debug
 
 debug = Debug(__name__)
 
-
-def layer_key_validator(layer, kc=None):
-    '''
-    Validates the syntax (but not semantics) of a layer key call.  We won't
-    have access to the keymap here, so we can't verify much of anything useful
-    here (like whether the target layer actually exists). The spirit of this
-    existing is mostly that Python will catch extraneous args/kwargs and error
-    out.
-    '''
-    return LayerKeyMeta(layer, kc)
-
-
-def layer_key_validator_lt(layer, kc, prefer_hold=False, **kwargs):
-    return HoldTapKeyMeta(tap=kc, hold=KC.MO(layer), prefer_hold=prefer_hold, **kwargs)
-
-
-def layer_key_validator_tt(layer, prefer_hold=True, **kwargs):
-    return HoldTapKeyMeta(
-        tap=KC.TG(layer), hold=KC.MO(layer), prefer_hold=prefer_hold, **kwargs
-    )
-
-
+# Define a LayerKeyMeta class to hold the layer and key code information
 class LayerKeyMeta:
     def __init__(self, layer, kc=None):
         self.layer = layer
         self.kc = kc
 
 
+# Define the Layers class, which inherits from HoldTap
 class Layers(HoldTap):
-    '''Gives access to the keys used to enable the layer system'''
-
     _active_combo = None
 
-    def __init__(
-        self,
-        combo_layers=None,
-    ):
-        # Layers
+    def __init__(self, combo_layers=None):
         super().__init__()
         self.combo_layers = combo_layers
+        self._init_argumented_keys()
+
+    def _init_argumented_keys(self):
+        # Keycodes: MO, DF, LM, TG, TO, LT, TT
+        # Each keycode has its corresponding validator and event handler functions
+        # MO - Momentary layer switch
+        # DF - Set default layer
+        # LM - Momentary layer switch with modifier
+        # TG - Toggle layer on or off
+        # TO - Temporary layer switch (disables all other layers)
+        # LT - Momentary layer switch or tap key
+        # TT - Toggle layer or tap key
         make_argumented_key(
-            validator=layer_key_validator,
+            validator=self.layer_key_validator,
             names=('MO',),
             on_press=self._mo_pressed,
             on_release=self._mo_released,
         )
         make_argumented_key(
-            validator=layer_key_validator, names=('DF',), on_press=self._df_pressed
+            validator=self.layer_key_validator, names=('DF',), on_press=self._df_pressed
         )
         make_argumented_key(
-            validator=layer_key_validator,
+            validator=self.layer_key_validator,
             names=('LM',),
             on_press=self._lm_pressed,
             on_release=self._lm_released,
         )
         make_argumented_key(
-            validator=layer_key_validator, names=('TG',), on_press=self._tg_pressed
+            validator=self.layer_key_validator, names=('TG',), on_press=self._tg_pressed
         )
         make_argumented_key(
-            validator=layer_key_validator, names=('TO',), on_press=self._to_pressed
+            validator=self.layer_key_validator, names=('TO',), on_press=self._to_pressed
         )
         make_argumented_key(
-            validator=layer_key_validator_lt,
+            validator=self.layer_key_validator_lt,
             names=('LT',),
             on_press=self.ht_pressed,
             on_release=self.ht_released,
         )
         make_argumented_key(
-            validator=layer_key_validator_tt,
+            validator=self.layer_key_validator_tt,
             names=('TT',),
             on_press=self.ht_pressed,
             on_release=self.ht_released,
         )
 
+    # Validator functions for each keycode
+    def layer_key_validator(self, layer, kc=None):
+        return LayerKeyMeta(layer, kc)
+
+    def layer_key_validator_lt(self, layer, kc, prefer_hold=False, **kwargs):
+        return HoldTapKeyMeta(
+            tap=kc, hold=KC.MO(layer), prefer_hold=prefer_hold, **kwargs
+        )
+
+    def layer_key_validator_tt(self, layer, prefer_hold=True, **kwargs):
+        return HoldTapKeyMeta(
+            tap=KC.TG(layer), hold=KC.MO(layer), prefer_hold=prefer_hold, **kwargs
+        )
+
+    # Event handler functions for each keycode (_xx_pressed, _xx_released)
     def _df_pressed(self, key, keyboard, *args, **kwargs):
-        '''
-        Switches the default layer
-        '''
         self.activate_layer(keyboard, key.meta.layer, as_default=True)
 
     def _mo_pressed(self, key, keyboard, *args, **kwargs):
-        '''
-        Momentarily activates layer, switches off when you let go
-        '''
         self.activate_layer(keyboard, key.meta.layer)
 
     def _mo_released(self, key, keyboard, *args, **kwargs):
         self.deactivate_layer(keyboard, key.meta.layer)
 
     def _lm_pressed(self, key, keyboard, *args, **kwargs):
-        '''
-        As MO(layer) but with mod active
-        '''
         keyboard.hid_pending = True
         keyboard.keys_pressed.add(key.meta.kc)
         self.activate_layer(keyboard, key.meta.layer)
 
     def _lm_released(self, key, keyboard, *args, **kwargs):
-        '''
-        As MO(layer) but with mod active
-        '''
         keyboard.hid_pending = True
         keyboard.keys_pressed.discard(key.meta.kc)
         self.deactivate_layer(keyboard, key.meta.layer)
 
     def _tg_pressed(self, key, keyboard, *args, **kwargs):
-        '''
-        Toggles the layer (enables it if not active, and vise versa)
-        '''
-        # See mo_released for implementation details around this
         if key.meta.layer in keyboard.active_layers:
             self.deactivate_layer(keyboard, key.meta.layer)
         else:
             self.activate_layer(keyboard, key.meta.layer)
 
     def _to_pressed(self, key, keyboard, *args, **kwargs):
-        '''
-        Activates layer and deactivates all other layers
-        '''
         self._active_combo = None
         keyboard.active_layers.clear()
         keyboard.active_layers.insert(0, key.meta.layer)
-
-    def _print_debug(self, keyboard):
-        if debug.enabled:
-            debug(f'active_layers={keyboard.active_layers}')
 
     def activate_layer(self, keyboard, layer, as_default=False):
         if as_default:
@@ -167,11 +144,7 @@ class Layers(HoldTap):
             return
 
         for combo, result in self.combo_layers.items():
-            matching = True
-            for layer in combo:
-                if layer not in keyboard.active_layers:
-                    matching = False
-                    break
+            matching = all(layer in keyboard.active_layers for layer in combo)
 
             if matching:
                 self._active_combo = combo
@@ -182,3 +155,7 @@ class Layers(HoldTap):
         if self._active_combo and layer in self._active_combo:
             keyboard.active_layers.remove(self.combo_layers[self._active_combo])
             self._active_combo = None
+
+    def _print_debug(self, keyboard):
+        if debug.enabled:
+            debug(f'active_layers={keyboard.active_layers}')
