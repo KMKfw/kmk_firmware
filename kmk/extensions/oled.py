2fc679f3d7c0922d3f91b8bccb223f64a18a254c
regicidal.plutophage <36969337+regicidalplutophage@users.noreply.github.com>
2023-03-25 01:06:24 +0300

    Implement suggested changes

--- kmk/extensions/oled.py
+++ kmk/extensions/oled.py
@@ -9,9 +9,11 @@ from adafruit_display_text import label
 from kmk.extensions import Extension
 from kmk.handlers.stock import passthrough as handler_passthrough
 from kmk.keys import make_key
-from kmk.kmktime import check_deadline
+from kmk.kmktime import PeriodicTimer, ticks_diff
 from kmk.modules.split import Split, SplitSide
 from kmk.utils import clamp
+
+displayio.release_displays()
 
 
 class TextEntry:
@@ -37,11 +39,9 @@ class TextEntry:
         self.background_color = 0x000000
         self.x_anchor = 0.0
         self.y_anchor = 0.0
-        self.x = x
-        self.y = y
         if x_anchor == 'L':
             self.x_anchor = 0.0
-            self.x = self.x + 1
+            x = x + 1
         if x_anchor == 'M':
             self.x_anchor = 0.5
         if x_anchor == 'R':
@@ -53,7 +53,7 @@ class TextEntry:
         if y_anchor == 'B':
             self.y_anchor = 1.0
         self.anchor_point = (self.x_anchor, self.y_anchor)
-        self.anchored_position = (self.x, self.y)
+        self.anchored_position = (x, y)
         if inverted:
             self.color = 0x000000
             self.background_color = 0xFFFFFF
@@ -93,8 +93,10 @@ class Oled(Extension):
         brightness=0.8,
         brightness_step=0.1,
         dim_time=20,
+        dim_target=0.1,
         off_time=60,
         powersave_dim_time=10,
+        powersave_dim_target=0.1,
         powersave_off_time=30,
     ):
         self.device_address = device_address
@@ -110,12 +112,14 @@ class Oled(Extension):
         self.timer_start = ticks_ms()
         self.powersave = False
         self.dim_time_ms = dim_time * 1000
+        self.dim_target = dim_target
         self.off_time_ms = off_time * 1000
         self.powersavedim_time_ms = powersave_dim_time * 1000
+        self.powersave_dim_target = powersave_dim_target
         self.powersave_off_time_ms = powersave_off_time * 1000
+        self.dim_period = PeriodicTimer(50)
         self.split_side = None
         # i2c initialization
-        displayio.release_displays()
         self.i2c = i2c
         if self.i2c is None:
             self.i2c = busio.I2C(scl, sda)
@@ -131,7 +135,7 @@ class Oled(Extension):
             on_release=handler_passthrough,
         )
 
-    def render_oled(self, layer):
+    def render(self, layer):
         splash = displayio.Group()
 
         for entry in self.entries:
@@ -161,9 +165,6 @@ class Oled(Extension):
                     )
                 )
         self.display.show(splash)
-
-    def updateOLED(self, sandbox):
-        self.render_oled(sandbox.active_layers[0])
 
     def on_runtime_enable(self, sandbox):
         return
@@ -195,14 +196,15 @@ class Oled(Extension):
         )
 
     def before_matrix_scan(self, sandbox):
-        self.dim()
+        if self.dim_period.tick():
+            self.dim()
         if sandbox.active_layers[0] != self.prev_layer:
             self.prev_layer = sandbox.active_layers[0]
-            self.updateOLED(sandbox)
+            self.render(sandbox.active_layers[0])
 
-    def after_matrix_scan(self, keyboard):
-        if keyboard.matrix_update or keyboard.secondary_matrix_update:
-            self.timer_time_reset()
+    def after_matrix_scan(self, sandbox):
+        if sandbox.matrix_update or sandbox.secondary_matrix_update:
+            self.timer_start = ticks_ms()
 
     def before_hid_send(self, sandbox):
         return
@@ -232,41 +234,38 @@ class Oled(Extension):
         )
         self.brightness = self.display.brightness  # Save current brightness
 
-    def timer_time_reset(self):
-        self.timer_start = ticks_ms()
-
     def dim(self):
         if self.powersave:
 
-            if self.powersave_off_time_ms != 0 and not check_deadline(
-                ticks_ms(), self.timer_start, self.powersave_off_time_ms
+            if (
+                self.powersave_off_time_ms
+                and ticks_diff(ticks_ms(), self.timer_start)
+                > self.powersave_off_time_ms
             ):
                 self.display.sleep()
 
-            elif self.powersave.dim_time_ms != 0 and not check_deadline(
-                ticks_ms(), self.timer_start, self.powersave_dim_time_ms
+            elif (
+                self.powersave_dim_time_ms
+                and ticks_diff(ticks_ms(), self.timer_start)
+                > self.powersave_dim_time_ms
             ):
-                self.display.brightness = (
-                    self.display.brightness / 2
-                    if self.display.brightness > 0.5
-                    else 0.1
-                )
+                self.display.brightness = self.powersave_dim_target
 
             else:
                 self.display.brightness = self.brightness
                 self.display.wake()
 
-        elif self.off_time_ms != 0 and not check_deadline(
-            ticks_ms(), self.timer_start, self.off_time_ms
+        elif (
+            self.off_time_ms
+            and ticks_diff(ticks_ms(), self.timer_start) > self.off_time_ms
         ):
             self.display.sleep()
 
-        elif self.dim_time_ms != 0 and not check_deadline(
-            ticks_ms(), self.timer_start, self.dim_time_ms
+        elif (
+            self.dim_time_ms
+            and ticks_diff(ticks_ms(), self.timer_start) > self.dim_time_ms
         ):
-            self.display.brightness = (
-                self.display.brightness / 2 if self.display.brightness > 0.5 else 0.1
-            )
+            self.display.brightness = self.dim_target
 
         else:
             self.display.brightness = self.brightness
