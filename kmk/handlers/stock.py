8638231e09017095149d627d10357704a7bb9b9b
xs5871 <xs5871@qn-4.net>
2023-04-01 00:08:41 +0000

    Add missing keycode

--- kmk/handlers/stock.py
+++ kmk/handlers/stock.py
@@ -137,3 +137,10 @@ def ble_disconnect(key, keyboard, *args, **kwargs):
 
     keyboard._hid_helper.clear_bonds()
     return keyboard
+
+
+def any_pressed(key, keyboard, *args, **kwargs):
+    from random import randint
+
+    key.code = randint(4, 56)
+    default_pressed(key, keyboard, *args, **kwargs)
