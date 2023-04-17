e8d0385b06c13278bd075d496d21f3bfc03321e5
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-17 22:11:12 +0100

    revision(s) to `4x6` files

--- boards/dactyl_manuform/4x6/main.py
+++ boards/dactyl_manuform/4x6/main.py
@@ -1,12 +1,19 @@
 from kb import KMKKeyboard
 from kmk.keys import KC
 from kmk.modules.layers import Layers
-from kmk.modules.split import Split, SplitType
+from kmk.modules.split import Split, SplitType, SplitSide
 from kmk.extensions.media_keys import MediaKeys
 
 keyboard = KMKKeyboard()
-keyboard.modules.append(Layers())
-keyboard.modules.append(Split())
+
+layers_ext = Layers()
+keyboard.modules.append(layers_ext)
+
+split = Split(
+    data_pin=keyboard.data_pin
+)
+keyboard.modules.append(split)
+
 keyboard.extensions.append(MediaKeys())
 
 keyboard.keymap = [
@@ -17,27 +24,28 @@ keyboard.keymap = [
                          KC.LEFT, KC.RGHT,                                                                                  KC.UP, KC.DOWN,
                                                    KC.MO(1),  KC.SPC,                          KC.ENT, KC.MO(2),
                                                              KC.HOME, KC.BSPC,        KC.DEL,  KC.END,
-                                                             KC.LCTL, KC.LGUI,       KC.LALT, KC.RCTL,
+                                                             KC.LCTL, KC.LGUI,       KC.LALT, KC.RCTL
     ],
     [  #NP_FN
-        KC.VOLU,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.INS,                                            KC.TRNS,  KC.P7,  KC.P8,   KC.P9,  KC.PMNS,  KC.TRNS,
-        KC.MUTE,  KC.F5,   KC.F6,   KC.F7,   KC.F8,  KC.PSCR,                                            KC.PAST,  KC.P4,  KC.P5,   KC.P6,  KC.PPLS,  KC.NLCK,
-        KC.VOLD,  KC.F9,  KC.F10,  KC.F11,  KC.F12,  KC.PAUS,                                            KC.PSLS,  KC.P1,  KC.P2,   KC.P3,  KC.PENT,  KC.TRNS,
-                         KC.BRID, KC.BRIU,                                                                                 KC.P0, KC.PDOT,
-                                                     KC.TRNS, KC.TRNS,                          KC.LGUI, KC.LCTL,
-                                                              KC.TRNS, KC.TRNS,        KC.TRNS, KC.TRNS,
-                                                              KC.TRNS, KC.TRNS,       KC.RESET,  KC.RLD,
+        KC.VOLU,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.INS,                                        KC.TRNS,  KC.P7,  KC.P8,   KC.P9,  KC.PMNS,  KC.TRNS,
+        KC.MUTE,  KC.F5,   KC.F6,   KC.F7,   KC.F8,  KC.PSCR,                                        KC.PAST,  KC.P4,  KC.P5,   KC.P6,  KC.PPLS,  KC.NLCK,
+        KC.VOLD,  KC.F9,  KC.F10,  KC.F11,  KC.F12,  KC.PAUS,                                        KC.PSLS,  KC.P1,  KC.P2,   KC.P3,  KC.PENT,  KC.TRNS,
+                         KC.BRID, KC.BRIU,                                                                             KC.P0, KC.PDOT,
+                                                     KC.TRNS, KC.NO,                        KC.LGUI, KC.LCTL,
+                                                              KC.NO, KC.NO,        KC.TRNS, KC.TRNS,
+                                                              KC.NO, KC.NO,       KC.RESET,  KC.RLD
     ],
     [  #NUM_SYM
-        KC.TRNS, KC.EXLM,   KC.AT,  KC.HASH,  KC.DLR, KC.PERC,                                              KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.TRNS,
-        KC.TRNS,   KC.N1,   KC.N2,    KC.N3,   KC.N4,   KC.N5,                                                KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0, KC.TRNS,
-        KC.TRNS,  KC.GRV, KC.MINS,  KC.PLUS, KC.LBRC, KC.LCBR,                                              KC.RCBR, KC.RBRC, KC.UNDS,  KC.EQL, KC.TILD, KC.TRNS,
-                          KC.PGDN,  KC.PGUP,                                                                                  KC.TRNS, KC.SLCK,
-                                                      KC.RCTL,   KC.LALT,                          KC.TRNS, KC.TRNS,
-                                                               KC.BT_PRV, KC.BT_NXT,      KC.TRNS, KC.TRNS,
-                                                               KC.BT_CLR,   KC.TRNS,      KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.EXLM,   KC.AT, KC.HASH,  KC.DLR, KC.PERC,                                           KC.CIRC, KC.AMPR, KC.ASTR,  KC.GRV, KC.TILD, KC.TRNS,
+        KC.TRNS,   KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                                             KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0, KC.TRNS,
+        KC.TRNS, KC.MINS, KC.PLUS, KC.LBRC, KC.LPRN, KC.LCBR,                                           KC.RCBR, KC.RPRN, KC.RBRC, KC.UNDS,  KC.EQL, KC.TRNS,
+                          KC.PGDN, KC.PGUP,                                                                               KC.TRNS, KC.SLCK,
+                                                      KC.RCTL,   KC.LALT,                        KC.NO, KC.TRNS,
+                                                               KC.BT_PRV, KC.BT_NXT,      KC.NO, KC.NO,
+                                                               KC.BT_CLR,   KC.TRNS,      KC.NO, KC.NO
     ]
 ]
 
 if __name__ == '__main__':
     keyboard.go()
+    
--- boards/dactyl_manuform/readme.md
+++ boards/dactyl_manuform/readme.md
@@ -1,11 +1,39 @@
 # Dactyl Manuform
 
+The [Dactyl-Manuform](https://github.com/tshort/dactyl-keyboard) is a split bodied, tented, curved keywell keyboard adapted from the [Dactyl](https://github.com/adereth/dactyl-keyboard) keyboard with the thumb cluster design of the [ManuForm](https://geekhack.org/index.php?topic=46015.0) keyboard. Information for building a Dactly Manuform is in first link.
 
+![Imgur](https://i.imgur.com/7y0Vbyd.jpg)
+*Two Dactyl Manuform 4x6 variants*
+
+## Variants
+
+Dactyl Manuform's are built in variations that cater for different row and column counts, and thumb clusters.  
+
+As standard: 
+- The finger keywell bottom row has 2 keys, 1 each in ring and middle columns
+    - Exception to this rule is the 7 column variants, that have two additional keys in this row
+- The thumb cluster has 6 keys, arranged in 2 columns by 3 rows 
+
+Variants are denoted as `RowCount`*x*`ColumnCount`
+
+### Rows
+**4**: 3 rows typically for Alpha numerics with 2 key (finger keywell) bottom row  
+**5**: 4 row with number row above  
+**6**: 5 row with function row above   
+
+### Columns
+**5** - A column for each finger with additional column for first finger  
+**6** - 5 column with additional pinky finger column  
+**7** - 6 column with additional first finger column
 
 ## Microcontroller support
 
 Amend the following line in variant's `kb.py` file to a supported microcontroller listed in `kmk/quickpin/pro_micro`:
 
 ```python
-from kmk.quickpin.pro_micro.boardsource_blok import pinout as pins
+from kmk.quickpin.pro_micro.controller import pinout as pins
+```
+for example, nice!nano controller(s) 
+```python
+from kmk.quickpin.pro_micro.nice_nano import pinout as pins
 ```
