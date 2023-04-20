ed3ddd2c4c7977002f5597e81fd96272f9976706
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-20 11:31:22 +0100

    human readable formatting

--- boards/dactyl/main.py
+++ boards/dactyl/main.py
@@ -35,7 +35,7 @@ keyboard.keymap = [
         KC.LCTL, KC.MINS, KC.EQL, KC.LEFT, KC.RGHT,                                                       KC.UP, KC.DOWN, KC.LBRC, KC.RBRC, KC.RCTL,
                                                            KC.LALT, KC.LGUI,    KC.RGUI, KC.RALT,
                                                                     KC.HOME,    KC.PGUP,
-                                                   L1_SPC,    S_BSPC, M_END,     H_PGDN,   C_DEL, L2_ENT
+                                                   L1_SPC,  S_BSPC,   M_END,     H_PGDN,   C_DEL, L2_ENT
     ],
     [   #1
         KC.F1,     KC.F2,  KC.F3,   KC.F4,   KC.F5, KC.F6,                                      KC.F7,   KC.F8, KC.F9,  KC.F10,  KC.F11,  KC.F12,
