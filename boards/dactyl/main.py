e9349ec3c399b51f70994dbd44af9e122a6d4f7a
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-20 11:29:02 +0100

    human readable formatting

--- boards/dactyl/main.py
+++ boards/dactyl/main.py
@@ -28,14 +28,14 @@ H_PGDN = KC.MT(KC.PGDN, KC.HYPER)
 
 keyboard.keymap = [
     [   #0
-        KC.ESC,    KC.N1,  KC.N2,   KC.N3,   KC.N4, KC.N5,                                          KC.N6, KC.N7,   KC.N8,   KC.N9,   KC.N0,  KC.GRV,
-        KC.TAB,     KC.Q,   KC.W,    KC.E,    KC.R,  KC.T,                                           KC.Y,  KC.U,    KC.I,    KC.O,    KC.P, KC.BSLS,
-        KC.CAPS,    KC.A,   KC.S,    KC.D,    KC.F,  KC.G,                                           KC.H,  KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,
-        KC.LSFT,    KC.Z,   KC.X,    KC.C,    KC.V,  KC.B,                                           KC.N,  KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.RSFT,
-        KC.LCTL, KC.MINS, KC.EQL, KC.LEFT, KC.RGHT,                                                        KC.UP, KC.DOWN, KC.LBRC, KC.RBRC, KC.RCTL,
+        KC.ESC,    KC.N1,  KC.N2,   KC.N3,   KC.N4, KC.N5,                                         KC.N6, KC.N7,   KC.N8,   KC.N9,   KC.N0,  KC.GRV,
+        KC.TAB,     KC.Q,   KC.W,    KC.E,    KC.R,  KC.T,                                          KC.Y,  KC.U,    KC.I,    KC.O,    KC.P, KC.BSLS,
+        KC.CAPS,    KC.A,   KC.S,    KC.D,    KC.F,  KC.G,                                          KC.H,  KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,
+        KC.LSFT,    KC.Z,   KC.X,    KC.C,    KC.V,  KC.B,                                          KC.N,  KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.RSFT,
+        KC.LCTL, KC.MINS, KC.EQL, KC.LEFT, KC.RGHT,                                                       KC.UP, KC.DOWN, KC.LBRC, KC.RBRC, KC.RCTL,
                                                            KC.LALT, KC.LGUI,    KC.RGUI, KC.RALT,
-                                                                    KC.HOME,    KC.PGUP,  
-                                                   L1_SPC, S_BSPC, M_END,    H_PGDN, C_DEL, L2_ENT
+                                                                    KC.HOME,    KC.PGUP,
+                                                   L1_SPC,    S_BSPC, M_END,     H_PGDN,   C_DEL, L2_ENT
     ],
     [   #1
         KC.F1,     KC.F2,  KC.F3,   KC.F4,   KC.F5, KC.F6,                                      KC.F7,   KC.F8, KC.F9,  KC.F10,  KC.F11,  KC.F12,
