955c5d58d3fdc8778a304e762c1afdeed31743e7
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-23 21:23:14 +0100

    Update main.py

--- boards/draculad/main.py
+++ boards/draculad/main.py
@@ -85,7 +85,7 @@ keyboard.keymap = [
         KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                         KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,
         KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                         KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN,
         KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                         KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH,
-                                    KC.LCTL,   LOWER,  KC.SPC,     KC.BSPC,    RAISE,  KC.ENT
+                                KC.LCTL,   LOWER,  KC.SPC,     KC.BSPC,  RAISE,   KC.ENT
 
     ],
     [  #RAISE
