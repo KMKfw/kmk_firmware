5b8396a07a654c46fc815abbd751cb88f9ff882e
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-24 12:06:16 +0100

    linting ignore added

--- boards/dactyl/kb.py
+++ boards/dactyl/kb.py
@@ -28,7 +28,9 @@ class KMKKeyboard(_KMKKeyboard):
     # data_pin2 =
     # rgb_pixel_pin = pins[avr['D3']]
     # num_pixels = 12
-
+    
+    # flake8: noqa
+    # fmt: off
     coord_mapping = [
         0,  1,  2,  3,  4,  5,                      41, 40, 39, 38, 37, 36,
         6,  7,  8,  9, 10, 11,                      47, 46, 45, 44, 43, 42,
