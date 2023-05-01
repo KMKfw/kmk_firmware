71dc239e531bd1eb15f90fccbb3d5ccc144bebf2
Duncan Sutherland <75046609+dunk2k@users.noreply.github.com>
2023-05-01 15:02:15 +0100

    Update main.py

--- boards/gtips/reviung41/main.py
+++ boards/gtips/reviung41/main.py
@@ -41,25 +41,25 @@ RGB_VAI = KC.RGB_VAI
 RGB_VAD = KC.RGB_VAD
 
 keyboard.keymap = [
-    [  # QWERTY
+    [   # QWERTY
         KC.TAB,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                         KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,  KC.BSPC,
         KC.LCTL,   KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                         KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,
         KC.LSFT,   KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                         KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, RSFT_ENT,
                                             KC.LALT,   LOWER,       KC.SPC,          RAISE,   KC.RGUI,
     ],
-    [  # LOWER
+    [   # LOWER
         KC.ESC,   KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                        KC.N6,   KC.N7,  KC.N8,   KC.N9,   KC.N0, KC.BSPC,
         KC.LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT, XXXXXXX, XXXXXXX,
         KC.LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RSFT_SPC,
                                             KC.LALT,   LOWER,       KC.SPC,         RAISE,   KC.RGUI,
     ],
-    [  # RAISE
+    [   # RAISE
         KC.ESC, KC.EXLM,   KC.AT, KC.HASH,  KC.DLR, KC.PERC,                      KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.BSPC,
         KC.LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC.MINS, KC.EQL, KC.LCBR, KC.RCBR, KC.PIPE,  KC.GRV,
         KC.LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC.UNDS, KC.PLUS, KC.LBRC, KC.RBRC, KC.BSLS, KC.TILD,
                                             KC.LALT,   LOWER,       KC.SPC,       RAISE,   KC.RGUI,
     ],
-    [  # ADJUST
+    [   # ADJUST
         RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
