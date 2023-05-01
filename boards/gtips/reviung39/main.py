63f91d26419f0609782dfe0fe294237eb42388f6
Duncan Sutherland <75046609+dunk2k@users.noreply.github.com>
2023-05-01 15:01:08 +0100

    Update main.py

--- boards/gtips/reviung39/main.py
+++ boards/gtips/reviung39/main.py
@@ -30,20 +30,20 @@ LOWER = KC.MO(1)
 RAISE = KC.MO(2)
 
 keyboard.keymap = [
-    [  #QWERTY
+    [   # QWERTY
         KC.ESC,   KC.N1,  KC.N2,   KC.N3,   KC.N4,   KC.N5,                    KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.GRV,
         KC.TAB,   KC.Q,   KC.W,    KC.E,    KC.R,    KC.T,                     KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.MINS,
         KC.LCTRL, KC.A,   KC.S,    KC.D,    KC.F,    KC.G,                     KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, KC.QUOT,
         KC.LSFT,  KC.Z,   KC.X,    KC.C,    KC.V,    KC.B,  KC.LBRC, KC.RBRC,  KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.SLSH, KC.RSFT,
                                    KC.LALT, KC.LGUI, LOWER, KC.SPC,  KC.ENT,   RAISE,  KC.BSPC, KC.RGUI,
     ],
-    [  #LOWER
+    [   # LOWER
         KC.F1,  KC.F2,    KC.F3,   KC.F4,   KC.F5,   KC.F6,                     KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,
         KC.GRV, KC.EXLM,  KC.AT,   KC.HASH, KC.DLR,  KC.PERC,                   KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.TILD,
         _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC.UNDS, KC.PLUS, KC.LCBR, KC.RCBR, KC.PIPE,
                                    _______, _______, _______, _______, _______, _______, _______, _______,
     ],
-    [  #RAISE
+    [   # RAISE
         KC.GRV, KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                       KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   _______,
         KC.F1,  KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,                       XXXXXXX, KC.LEFT, KC.DOWN, KC.UP,   KC.RGHT, XXXXXXX,
         KC.F7,  KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,   _______, _______,  KC.PLUS, KC.MINS, KC.EQL,  KC.LBRC, KC.RBRC, KC.BSLS,
