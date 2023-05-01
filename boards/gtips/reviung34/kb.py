cbefa4812c357495de7021763ad0f4761dac8440
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-05-01 15:55:08 +0100

    Update kb.py

--- boards/gtips/reviung34/kb.py
+++ boards/gtips/reviung34/kb.py
@@ -6,12 +6,7 @@ from kmk.scanners import DiodeOrientation
 
 
 class KMKKeyboard(_KMKKeyboard):
-    row_pins = (
-        pins[19],
-        pins[18],
-        pins[17],
-        pins[16]
-    )
+    row_pins = (pins[19], pins[18], pins[17], pins[16])
     col_pins = (
         pins[6],
         pins[7],
@@ -21,7 +16,7 @@ class KMKKeyboard(_KMKKeyboard):
         pins[15],
         pins[14],
         pins[13],
-        pins[12]
+        pins[12],
     )
     diode_orientation = DiodeOrientation.COLUMNS
     i2c = board.I2C
@@ -30,9 +25,11 @@ class KMKKeyboard(_KMKKeyboard):
     led_key_pos = [0, 1, 2, 3, 8, 4, 5, 6, 7]
     brightness_limit = 1.0
     num_pixels = 9
+    # flake8: noqa
+    # fmt: off
     coord_mapping = [
         0,   1,  2,  3,  4,  5,  6,  7,  8, 32,
         9,  10, 11, 12, 13, 14, 15, 16, 17, 33,
         18, 19, 20, 21, 22, 23, 24, 25, 26, 34,
-        29, 30, 31, 35,
+        29, 30, 31, 35
     ]
