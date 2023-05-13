6018b2f958cde84e5a01b7fcea548b61c0afd191
Duncan Sutherland <75046609+dunk2k@users.noreply.github.com>
2023-05-13 23:04:46 +0100

    `Draculad` board: corrections applied to pass linter (#767)

--- boards/draculad/README.md
+++ boards/draculad/README.md
@@ -1,23 +1,24 @@
-# Draculad
+# DracuLad
 
+The [DracuLad](https://github.com/MangoIV/dracuLad) is a feature-packed 30% sized, split bodied, columnar staggered keyboard.
 
-The Draculad is is a feature-packed 30% split columnar staggered keyboard.
+![KeycaplessTopElev](https://github.com/mangoiv/draculad/raw/master/pictures/rev1/both_sides_underglow_oleds.jpg)
+
 Retailers (USA)  
 [Boardsource](https://boardsource.xyz/store)  
 
 Extensions enabled by default  
 - [Layers](/docs/en/layers.md) Need more keys than switches? Use layers.
-- [ModTap](/docs/en/modtap.md) Allows mod keys to act as different keys when tapped.
-- [Split](/docs/en/split_keyboards.md) Connects halves using a wire
+- [Split](/docs/en/split_keyboards.md) Connects halves using a wire.
 - [peg_RGB_matrix](/docs/en/peg_rgb_matrix.md) Allows mod keys to act as different keys when tapped.
-- [peg_oled_display](/docs/en/peg_oled_display.md) Connects halves using a wire
+- [peg_oled_display](/docs/en/peg_oled_display.md) Connects halves using a wire.
 
 Common Extensions
 - [Power](/docs/enpower.md) Powersaving features for battery life
 
 ## Microcontroller support
 
-Update this line in `kb.py` to any supported microcontroller in `kmk/quickpin/pro_micro`:
+Replace `boardsource_blok` in `kb.py` to any supported microcontroller in `kmk/quickpin/pro_micro`:
 
 ```python
 from kmk.quickpin.pro_micro.boardsource_blok import pinout as pins
--- boards/draculad/main.py
+++ boards/draculad/main.py
@@ -1,6 +1,6 @@
 from kb import KMKKeyboard
 
-from kmk.extensions.peg_oled_Display import (
+from kmk.extensions.peg_oled_display import (
     Oled,
     OledData,
     OledDisplayMode,
@@ -9,22 +9,64 @@ from kmk.extensions.peg_oled_Display import (
 from kmk.extensions.peg_rgb_matrix import Rgb_matrix
 from kmk.keys import KC
 from kmk.modules.layers import Layers
-from kmk.modules.modtap import ModTap
 from kmk.modules.split import Split
 
 keyboard = KMKKeyboard()
-keyboard.debug_enable=True
-
-modtap = ModTap()
-layers_ext = Layers()
-keyboard.modules.append(layers_ext)
-keyboard.modules.append(modtap)
+keyboard.debug_enable = True
+keyboard.modules.append(Layers())
 # oled
-oled_ext = Oled(OledData(corner_one={0:OledReactionType.STATIC,1:["1 2 3 4 5 6","","","","","","",""]},corner_two={0:OledReactionType.STATIC,1:[" 7 8 Layer","","","","","",""," 7 8 Layer"]},corner_three={0:OledReactionType.LAYER,1:["^","  ^","    ^","      ^","        ^","          ^","",""]},corner_four={0:OledReactionType.LAYER,1:["","","","","",""," ^","   ^"]}),toDisplay=OledDisplayMode.TXT,flip= True)
+oled_ext = Oled(
+    OledData(
+        corner_one={
+            0: OledReactionType.STATIC,
+            1: ['1 2 3 4 5 6', '', '', '', '', '', '', ''],
+        },
+        corner_two={
+            0: OledReactionType.STATIC,
+            1: [' 7 8 Layer', '', '', '', '', '', '', ' 7 8 Layer'],
+        },
+        corner_three={
+            0: OledReactionType.LAYER,
+            1: ['^', '  ^', '    ^', '      ^', '        ^', '          ^', '', ''],
+        },
+        corner_four={
+            0: OledReactionType.LAYER,
+            1: ['', '', '', '', '', '', ' ^', '   ^'],
+        },
+    ),
+    toDisplay=OledDisplayMode.TXT,
+    flip=True,
+)
 # oled
 keyboard.extensions.append(oled_ext)
 # ledmap
-rgb_ext = Rgb_matrix(ledDisplay=[[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255]],split=True,rightSide=False,disable_auto_write=True)
+rgb_ext = Rgb_matrix(
+    ledDisplay=[
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+    ],
+    split=True,
+    rightSide=False,
+    disable_auto_write=True,
+)
 # ledmap
 keyboard.extensions.append(rgb_ext)
 split = Split(use_pio=True)
@@ -37,27 +79,23 @@ RAISE = KC.MO(1)
 
 keyboard.keymap = [
     [  #QWERTY
-        KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                         KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,\
-        KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                         KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN,\
-        KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                         KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH,\
-                                    KC.LCTL,   LOWER,  KC.SPC,     KC.BSPC,    RAISE,  KC.ENT
-
+        KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                           KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,
+        KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                           KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN,
+        KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                           KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.SLSH,
+                                   KC.LCTL, LOWER,   KC.SPC,       KC.BSPC, RAISE,   KC.ENT,
     ],
     [  #RAISE
-        KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                        KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,\
-        KC.TAB,  KC.LEFT, KC.DOWN, KC.UP,   KC.RGHT,                      XXXXXXX, KC.MINS, KC.EQL,  KC.LBRC, KC.RBRC,\
-        KC.LCTL, KC.GRV,  KC.LGUI, KC.LALT, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, KC.BSLS, KC.QUOT,\
-                                    XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX
-
+        KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                          KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,
+        KC.TAB,  KC.LEFT, KC.DOWN, KC.UP,   KC.RGHT,                        XXXXXXX, KC.MINS, KC.EQL,  KC.LBRC, KC.RBRC,
+        KC.LCTL, KC.GRV,  KC.LGUI, KC.LALT, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, KC.BSLS, KC.QUOT,
+                                   XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX,
     ],
     [  #LOWER
-        KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC,      KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN,\
-        KC.ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, KC.UNDS, KC.PLUS, KC.LCBR, KC.RCBR,\
-        KC.CAPS, KC.TILD, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, KC.PIPE,  KC.DQT,\
-                            XXXXXXX, XXXXXXX, XXXXXXX,      KC.ENT,  XXXXXXX, KC.DEL
-
-
-    ]
+        KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC,                        KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN,
+        KC.ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, KC.UNDS, KC.PLUS, KC.LCBR, KC.RCBR,
+        KC.CAPS, KC.TILD, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, KC.PIPE, KC.DQT,
+                                   XXXXXXX, XXXXXXX, XXXXXXX,       KC.ENT, XXXXXXX, KC.DEL,
+    ],
 ]
 
 if __name__ == '__main__':
