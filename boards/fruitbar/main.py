1d606fd1f85130a4f9581897a65a13cc7860b364
newts <jvail@newts.org>
2023-07-31 12:39:53 -0400

    wip

--- boards/fruitbar/main.py
+++ boards/fruitbar/main.py
@@ -36,18 +36,18 @@ keyboard.keymap = [
         KC.LCTRL, KC.LGUI, KC.LALT, KC.NO,  KC.BSPC,KC.NO,  KC.NO, KC.SPC, KC.NO,  FN1,     FN2,     KC.NO,    KC.NO,   KC.LEFT, KC.DOWN,  KC.RGHT, 
     ],
     # Alt
-    # ,----------------------------------------------------------------------------.s
-    # | Grave|   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |
-    # |------+------+------+------+------+------+------+------+------+------+------|
-    # | Trans|   F1 |   F3 |   F5 |   F7 |   F9 |  F11 |   -  |   ;  |   '  | Trans|
-    # |------+------+------+------+------+-------------+------+------+------+------|
-    # | Trans|   F2 |   F4 |   F6 |   F8 |  F10 |  F12 |   =  |   (  | Pg Up|   )  |
-    # |------+------+------+------+------+------+------+------+------+------+------|
-    # | Trans| Trans| Trans| Trans| Trans| Trans|   \  | Home | Pg Dn|  End |  Del |
-    # `----------------------------------------------------------------------------'
+    # ,---------------------------------------------------------------------------------------------------------------.
+    # | Grave|   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |      |      |      |      |
+    # |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+    # | Trans|   !  |   @  |   #  |   $  |   %  |  ^   |   &  |   *  |   =  |   -  |      |      |      |      |      |
+    # |------+------+------+------+------+-------------+------+------+------+------+------+------+------+------+------|
+    # | Trans|   [  |   ]  |   (  |   _  |   )  |  F12 |   =  |   (  | Pg Up|   )  |      |      |      |      |      |
+    # |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+    # | Trans| Trans| Trans| Trans| Trans| Trans|   \  | Home | Pg Dn|  End |  Del |      |      |      |      |      |
+    # `---------------------------------------------------------------------------------------------------------------'
     [
         KC.GRV,  KC.N1,   KC.N2,   KC.N3,    KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
-        KC.TRNS, KC.F1,   KC.F3,   KC.F5,    KC.F7,   KC.F9,   KC.F11,  KC.MINS, KC.SCLN, KC.QUOT, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.EXLM, KC.AT,   KC.HASH,  KC.DLR,  KC.PERC, KC.CIRC, KC.AMPR, KC.ASTR, KC.EQL,  KC.MINS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
         KC.TRNS, KC.F2,   KC.F4,   KC.F6,    KC.F8,   KC.F10,  KC.F12,  KC.EQL,  KC.LBRC, KC.PGUP, KC.RBRC, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
         KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,  KC.TRNS, KC.TRNS, KC.BSLS, KC.HOME, KC.PGDN, KC.END,  KC.DEL,  KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
     ],
