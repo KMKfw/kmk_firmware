d56ba7cd4ca6d6268a357a79cccdeed8c919be4a
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-08-04 16:19:18 -0400

    add support for sh1106 oled displays

--- kmk/extensions/oled.py
+++ kmk/extensions/oled.py
@@ -110,6 +110,46 @@ class SSD1306(OledDisplayType):
         self.i2c.deinit()
 
 
+class SH1106(OledDisplayType):
+    def __init__(
+        self,
+        spi=None,
+        sck=None,
+        mosi=None,
+        command=None,
+        chip_select=None,
+        reset=None,
+        baudrate=1000000,
+    ):
+        self.command = command
+        self.chip_select = chip_select
+        self.reset = reset
+        self.baudrate = baudrate
+        # spi initialization
+        self.spi = spi
+        if self.spi is None:
+            self.spi = busio.SPI(sck, mosi)
+
+    def during_bootup(self, width, height, rotation):
+        import adafruit_displayio_sh1106
+
+        return adafruit_displayio_sh1106.SH1106(
+            displayio.FourWire(
+                self.spi,
+                command=self.command,
+                chip_select=self.chip_select,
+                reset=self.reset,
+                baudrate=self.baudrate,
+            ),
+            width=width,
+            height=height,
+            rotation=rotation,
+        )
+
+    def deinit(self):
+        self.spi.deinit()
+
+
 class Oled(Extension):
     def __init__(
         self,
