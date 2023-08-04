a71b6603edd5242f4af9e8d9ac0f91f1c15fc6ab
xs5871 <xs5871@qn-4.net>
2023-08-04 09:35:49 +0000

    Fixup

--- boards/a_dux/kb.py
+++ boards/a_dux/kb.py
@@ -5,19 +5,14 @@ from kmk.scanners.keypad import KeysScanner
 # GPIO to key mapping - each line is a new row.
 # fmt: off
 _KEY_CFG_LEFT = [
-     pins[7], pins[1], pins[16], pins[13], pins[19],
-    pins[8], pins[5], pins[17], pins[14],  pins[18],
-     pins[9],  pins[6],  pins[0],  pins[15],  pins[12],
-                                  pins[10], pins[11],
+    pins[7], pins[1], pins[16], pins[13], pins[19],
+    pins[8], pins[5], pins[17], pins[14], pins[18],
+    pins[9], pins[6], pins[0],  pins[15], pins[12],
+                                pins[10], pins[11],
 ]
-
-
 # fmt: on
 
 class KMKKeyboard(_KMKKeyboard):
-    def __init__(self):
-        # create and register the scanner
-        self.matrix = KeysScanner(_KEY_CFG_LEFT)
     data_pin = pins[4]
 
     # flake8: noqa
@@ -28,3 +23,7 @@ class KMKKeyboard(_KMKKeyboard):
     10, 11, 12, 13, 14,   31, 30, 29, 28, 27,
                 15, 16,   33, 32
     ]
+
+    def __init__(self):
+        # create and register the scanner
+        self.matrix = KeysScanner(_KEY_CFG_LEFT)
