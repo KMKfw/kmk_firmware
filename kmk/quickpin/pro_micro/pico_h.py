9bfd90e1cd624a04da1da273ec66a8756f0437f9
Allstreamer <48365544+Allstreamer@users.noreply.github.com>
2023-08-18 21:14:01 +0200

    Create pico_h.py

---
+++ kmk/quickpin/pro_micro/pico_h.py
@@ -0,0 +1,44 @@
+import board
+
+pinout = [
+    board.GP0,
+    board.GP1,
+    None,  # GND
+    board.GP2,
+    board.GP3,
+    board.GP4,
+    board.GP5,
+    None,  # GND
+    board.GP6,
+    board.GP7,
+    board.GP8,
+    board.GP9,
+    None,  # GND
+    board.GP10,
+    board.GP11,
+    board.GP12,
+    board.GP13,
+    None,  # GND
+    board.GP14,
+    board.GP15,
+    board.GP16,
+    board.GP17,
+    None,  # GND
+    board.GP18,
+    board.GP19,
+    board.GP20,
+    board.GP21,
+    None,  # GND
+    board.GP22,
+    None,  # RUN
+    board.GP26,
+    board.GP27,
+    None,  # GND, AGND
+    board.GP28,
+    None,  # ADC_VREF
+    None,  # 3V3 OUT
+    None,  # 3V3 EN
+    None,  # GND
+    None,  # VSYS
+    None,  # VBUS
+]
