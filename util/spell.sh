51c05b5112656f5640542da3e32a94eeeeb4c1b1
Karl Grasegger <gruenesgnu@posteo.de>
2023-08-19 12:42:32 +0200

    fix: Apply shellcheck suggestions

--- util/spell.sh
+++ util/spell.sh
@@ -5,10 +5,10 @@ if ! command -v aspell; then
   exit
 fi
 
-ROOT=`git rev-parse --show-toplevel`
+ROOT=$(git rev-parse --show-toplevel)
 
 # This gets us only english .md files at the moment, but is clearly brittle
-MARKDOWN_FILES=`find $ROOT -name '*.md' -not -path '**/ptBR/**' -not -path '**/*-ptBR.md' -not -path '**/ja/**'`
+MARKDOWN_FILES=$(find "$ROOT" -name '*.md' -not -path '**/ptBR/**' -not -path '**/*-ptBR.md' -not -path '**/ja/**')
 # Use our local dict for saved words, and use en_US for the main dict
 ASPELL="aspell -M --dont-save-repl -p $ROOT/util/aspell.en.pws -d en_US"
 
@@ -30,14 +30,14 @@ if $INTERACTIVE; then
   # run aspell interactively on each file
   for file in $MARKDOWN_FILES; do
     # echo "$ASPELL --dont-backup -c $file"
-    $ASPELL --dont-backup -c $file
+    $ASPELL --dont-backup -c "$file"
   done
 fi
 
 # non-interactive error report
 for file in $MARKDOWN_FILES; do
   # echo "cat $file | $ASPELL list"
-  BAD_WORDS=`cat $file | $ASPELL list`
+  BAD_WORDS=$($ASPELL list < "$file" )
   if [ -n "$BAD_WORDS" ]; then
     EXIT_STATUS=1
     echo "$file still has spelling errors. To correct spelling on only this file, run the command:"
