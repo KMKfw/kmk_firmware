2edd6d26dcb92d1916efcc856d4acb49f197a553
Allstreamer <48365544+Allstreamer@users.noreply.github.com>
2023-08-23 17:06:10 +0200

    Expand Scaffold

--- kmk/converters/qmk.py
+++ kmk/converters/qmk.py
@@ -1,4 +1,16 @@
+import json
+
 
 def read_qmk_config(path: str):
-    # TODO
-    return []
+    json_data = {}
+    with open(path, "r") as config_file_handle:
+        json_data = json.load(config_file_handle)
+    key_map_data = [
+        [parse_str_to_kmk_key(key) for key in layer] for layer in json_data["layers"]
+    ]
+    return json_data["layers"]
+
+
+def parse_str_to_kmk_key(key: str):
+    # TODO: Parse key strings
+    return key
