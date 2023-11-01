544ec9ccab8ef45aa8cc24dcab460c90ba4269cb
bobdoah <1266467+bobdoah@users.noreply.github.com>
2023-11-01 09:11:10 +0000

    Isopad (#908)
    * add isopad firmware

---
+++ boards/isopad/kb.py
@@ -0,0 +1,20 @@
+# Isopad pinout
+# Credit: u/bomtarnes aka The Keyboard Magpie
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard as _KMKeyboard
+from kmk.quickpin.pro_micro.kb2040 import pinout as pins
+from kmk.scanners.keypad import KeysScanner
+
+# GPIO to key mapping - there's one key
+# fmt: off
+_KEY_CFG = [
+        pins[19]
+]
+
+
+# fmt: on
+class KMKKeyboard(_KMKeyboard):
+    def __init__(self):
+        # create and register the scanner
+        self.matrix = KeysScanner(_KEY_CFG)
