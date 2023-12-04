562aef9255edb87053fa81690fdc895cb5117353
Patrick Surry <patrick.surry@gmail.com>
2023-12-04 07:37:36 -0500

    fix key names

--- boards/nullbitsco/tidbit/main.py
+++ boards/nullbitsco/tidbit/main.py
@@ -8,11 +8,11 @@ XXXXX = KC.NO
 
 keyboard.keymap = [
     [
-        XXXXX,  KC.PSLS, KC_PAST, KC_PMNS,
-        KC_P7,  KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
-        KC_P1,  KC_P2,   KC_P3,   KC_PENT,
-        KC_P0,  KC_P0,   KC_PDOT, KC_PENT,
+        XXXXX,  KC.PSLS, KC.PAST, KC.PMNS,
+        KC.P7,  KC.P8,   KC.P9,   KC.PPLS,
+        KC.P4,  KC.P5,   KC.P6,   KC.PPLS,
+        KC.P1,  KC.P2,   KC.P3,   KC.PENT,
+        KC.P0,  KC.P0,   KC.PDOT, KC.PENT,
     ]
 ]
 
