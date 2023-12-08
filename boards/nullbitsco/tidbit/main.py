1248236b8f4b61c29bb8b729d8b54bec58087551
patricksurry <patrick.surry@gmail.com>
2023-12-08 13:28:43 -0500

    Mapping for nullbits tidbit board with bit-c pro (#916)
    Adds nullbits tidbit board and bitc promicro mapping

---
+++ boards/nullbitsco/tidbit/main.py
@@ -0,0 +1,21 @@
+from kb import KMKKeyboard
+
+from kmk.keys import KC
+
+# add active_encoders=[0, 2] to constructor if first and third encoders installed
+keyboard = KMKKeyboard()
+
+XXXXX = KC.NO
+
+keyboard.keymap = [
+    [
+        XXXXX,  KC.PSLS, KC.PAST, KC.PMNS,
+        KC.P7,  KC.P8,   KC.P9,   KC.PPLS,
+        KC.P4,  KC.P5,   KC.P6,   KC.PPLS,
+        KC.P1,  KC.P2,   KC.P3,   KC.PENT,
+        KC.P0,  KC.P0,   KC.PDOT, KC.PENT,
+    ]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
