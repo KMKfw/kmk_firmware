8bfb08ca0dcff3e023ea79efd1bb8c78ead22deb
Patrick Surry <patrick.surry@gmail.com>
2023-12-09 08:37:19 -0500

    reorg

--- tests/mock_board.py
+++ tests/mock_board.py
@@ -2,26 +2,17 @@
 #   python -m tests.mock_board boards/nullbitsco/tidbit
 
 import sys
-from unittest.mock import Mock
 
-from .mocks import init_circuit_python_modules_mocks
+from .mocks import init_board_module_mocks
 
-init_circuit_python_modules_mocks()
 
-sys.modules['rp2pio'] = Mock()
-sys.modules['pwmio'] = Mock()
-sys.modules['rotaryio'] = Mock()
-sys.modules['displayio'] = Mock()
-sys.modules['terminalio'] = Mock()
-sys.modules['adafruit_pixelbuf'] = Mock()
-sys.modules['adafruit_pixelbuf'].PixelBuf = Mock()
-sys.modules['adafruit_displayio_ssd1306'] = Mock()
-sys.modules['adafruit_display_text'] = Mock()
+init_board_module_mocks()
 
-sys.path.insert(0, sys.argv[1])
+board_dir = sys.argv[1]
+sys.path.insert(0, board_dir)
 
 # TODO pass tests for dactyl keebs which seem to map a mystery pin B7
-if 'dactyl' in sys.argv[1]:
+if 'dactyl' in board_dir:
     from kmk.quickpin.pro_micro.avr_promicro import translate as avr
 
     avr['B7'] = -1
--- tests/mocks.py
+++ tests/mocks.py
@@ -36,3 +36,16 @@ def init_circuit_python_modules_mocks():
     from . import task
 
     sys.modules['_asyncio'] = task
+
+
+def init_board_module_mocks():
+    init_circuit_python_modules_mocks()
+    sys.modules['rp2pio'] = Mock()
+    sys.modules['pwmio'] = Mock()
+    sys.modules['rotaryio'] = Mock()
+    sys.modules['displayio'] = Mock()
+    sys.modules['terminalio'] = Mock()
+    sys.modules['adafruit_pixelbuf'] = Mock()
+    sys.modules['adafruit_pixelbuf'].PixelBuf = Mock()
+    sys.modules['adafruit_displayio_ssd1306'] = Mock()
+    sys.modules['adafruit_display_text'] = Mock()
