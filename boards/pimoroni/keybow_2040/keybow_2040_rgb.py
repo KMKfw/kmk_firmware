d2239352e588a85377a76f99ea273b6fa67de970
Phil Howard <github@gadgetoid.com>
2024-06-12 00:43:31 +0100

    Keybow 2040: drop double-buffering, simplify buffer output.
    Remove hot path conversion to bytes and drop double-buffering which had no visible impact.
    
    Reduce the size of the output buffer to remove leading 17 and trailing 4 bytes, and prime
    the buffer with the first LED address.
    
    Drop offset multiply and cache LED offsets in address lookup.
    
    Iterate through address lookup and unpack offset, r, g, b.
    
    Update time is now ~4ms and much more stable.
    
    Signed-off-by: Phil Howard <github@gadgetoid.com>

--- boards/pimoroni/keybow_2040/keybow_2040_rgb.py
+++ boards/pimoroni/keybow_2040/keybow_2040_rgb.py
@@ -5,7 +5,6 @@ from micropython import const
 from adafruit_bus_device.i2c_device import I2CDevice
 from adafruit_pixelbuf import PixelBuf
 
-_FRAME_REGISTER = const(0x01)
 _SHUTDOWN_REGISTER = const(0x0A)
 _CONFIG_BANK = const(0x0B)
 _BANK_ADDRESS = const(0xFD)
@@ -19,66 +18,60 @@ class Keybow2040Leds(PixelBuf):
 
     width = 16
     height = 3
+
+    # Pixel addresses starting from LED 17,
+    # then offset by 1 to fit register addr
     _pixel_addr = [
-        (120, 88, 104),  # 0, 0
-        (136, 40, 72),  # 1, 0
-        (112, 80, 96),  # 2, 0
-        (128, 32, 64),  # 3, 0
-        (121, 89, 105),  # 0, 1
-        (137, 41, 73),  # 1, 1
-        (113, 81, 97),  # 2, 1
-        (129, 33, 65),  # 3, 1
-        (122, 90, 106),  # 0, 2
-        (138, 25, 74),  # 1, 2
-        (114, 82, 98),  # 2, 2
-        (130, 17, 66),  # 3, 2
-        (123, 91, 107),  # 0, 3
-        (139, 26, 75),  # 1, 3
-        (115, 83, 99),  # 2, 3
-        (131, 18, 67),  # 3, 3
+        (0, 104, 72, 88),  # 0, 0
+        (3, 120, 24, 56),  # 1, 0
+        (6, 96, 64, 80),  # 2, 0
+        (9, 112, 16, 48),  # 3, 0
+        (12, 105, 73, 89),  # 0, 1
+        (15, 121, 25, 57),  # 1, 1
+        (18, 97, 65, 81),  # 2, 1
+        (21, 113, 17, 49),  # 3, 1
+        (24, 106, 74, 90),  # 0, 2
+        (27, 122, 9, 58),  # 1, 2
+        (30, 98, 66, 82),  # 2, 2
+        (33, 114, 1, 50),  # 3, 2
+        (36, 107, 75, 91),  # 0, 3
+        (39, 123, 10, 59),  # 1, 3
+        (42, 99, 67, 83),  # 2, 3
+        (45, 115, 2, 51),  # 3, 3
     ]
 
-    def __init__(self, size: int = 16):  # Kept for backward compatibility
+    def __init__(self, size: int = 16):  # size kept for backward compatibility
         self.i2c = busio.I2C(board.SCL, board.SDA, frequency=400_000)
         self.i2c_device = I2CDevice(self.i2c, 0x74)
-        self.out_buffer = bytearray(144)
+        self.out_buffer = bytearray(124)
         self._pixels = 16
-        self._frame = 0
         super().__init__(self._pixels, byteorder='RGB')
 
+        # First byte of buffer is our first LED address
+        self.out_buffer[0] = _COLOR_OFFSET + 17
+
         with self.i2c_device as i2c:
-            for frame in range(2):
-                i2c.write(bytes([_BANK_ADDRESS, frame]))
-                i2c.write(bytes([_ENABLE_OFFSET] + [0xFF] * 18))  # Enable all LEDs
-                i2c.write(bytes([_COLOR_OFFSET] + [0x00] * 144))  # Init all to 0
+            i2c.write(bytes([_BANK_ADDRESS, 0]))
+            i2c.write(bytes([_ENABLE_OFFSET] + [0xFF] * 18))  # Enable all LEDs
+            i2c.write(bytes([_COLOR_OFFSET] + [0x00] * 144))  # Init all to 0
 
             i2c.write(bytes([_BANK_ADDRESS, _CONFIG_BANK]))
             i2c.write(bytes([0x00] * 14))  # Clear Mode, Frame, ... etc
             i2c.write(bytes([_SHUTDOWN_REGISTER, 0x01]))  # 0 == shutdown, 1 == normal
 
+            i2c.write(bytes([_BANK_ADDRESS, 0]))
+
     def _transmit(self, buffer):
-        # Bring these into local scope for a tiny perf improvement ~.7ms
-        pixel_addr = self._pixel_addr
+        # Bring buffer into local scope for a tiny perf improvement
         out = self.out_buffer
         # Shuffle the 16 pixel PixelBuf buffer into our 144 LED
         # display native format.
-        for x in range(self._pixels):
-            r, g, b = pixel_addr[x]
-            out[r] = buffer[x * 3 + 0]
-            out[g] = buffer[x * 3 + 1]
-            out[b] = buffer[x * 3 + 2]
+        for o, r, g, b in self._pixel_addr:
+            out[r] = buffer[o + 0]
+            out[g] = buffer[o + 1]
+            out[b] = buffer[o + 2]
 
         with self.i2c_device as i2c:
-            # Switch to our new (not currently visible) frame
-            i2c.write(bytes([_BANK_ADDRESS, self._frame]))
-
-            # We only actually use 16 * 3 = 48 LEDs out of the 144 total
-            # but at 400KHz I2C it's cheaper just to write the whole lot
-            i2c.write(bytes([_COLOR_OFFSET + 17]) + self.out_buffer[17:140])
-
-            # Set the newly written frame as the visible one
-            i2c.write(bytes([_BANK_ADDRESS, _CONFIG_BANK]))
-            i2c.write(bytes([_FRAME_REGISTER, self._frame]))
-
-        # Switch to our other buffer
-        self._frame = not self._frame
+            # Write our 124 byte buffer
+            # byte 0 is prefilled with the register address
+            i2c.write(out)
