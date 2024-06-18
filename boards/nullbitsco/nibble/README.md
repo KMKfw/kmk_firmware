d053588be0464c32a65ee0e575373a61fd8c3ad1
Patrick Surry <patrick.surry@gmail.com>
2024-06-18 17:53:18 -0400

    working key mapping and encoder

--- boards/nullbitsco/nibble/README.md
+++ boards/nullbitsco/nibble/README.md
@@ -3,33 +3,8 @@ Keyboard mapping for the [nullbits nibble](https://nullbits.co/nibble/).
 Copy `kb.py` and `main.py` to your top level CircuitPython folder beside the kmk folder.
 Edit the key mapping in `main.py` to match your keyboard layout.
 
-The Keyboard constructor supports a couple of optional arguments (see `kb.py`).
+The Keyboard constructor supports an optional `encoder` argument (see `kb.py`).
+See the sample `main.py` for an example of how to configure encoder
 
-TODO
-
-You can specify the active encoder positions by passing a list like
-`active_encoders=[0, 2]` which corresponds to the 1st and 3rd positions shown
-in [step 6](https://github.com/nullbitsco/docs/blob/main/tidbit/build_guide_en.md#6-optional-solder-rotary-encoder-led-matrix-andor-oled-display) of the build guide.
-The default is for a single encoder in either of the top two locations labeled 1
-in the build diagram, i.e. `active_encoders=[0]`.  Pass an empty list if you skipped
-adding any encoders.
-
-You can control the RGB backlights with the [RGB extension](http://kmkfw.io/docs/rgb).
-Here's an example:
-
-```python
-from kb import KMKKeyboard
-from kmk.extensions.rgb import RGB, AnimationModes
-
-keyboard = KMKKeyboard(active_encoders=[0])
-
-rgb = RGB(
-    pixel_pin=keyboard.pixel_pin,
-    num_pixels=10,
-    animation_mode=AnimationModes.BREATHING,
-    animation_speed=3,
-    breathe_center=2,
-)
-keyboard.extensions.append(rgb)
-
-```
+The RGB extension in the example requires a copy of `neopixel.py` in your top level
+CircuitPython folder: see https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/rgb.md.
--- boards/nullbitsco/nibble/main.py
+++ boards/nullbitsco/nibble/main.py
@@ -1,19 +1,27 @@
 from kb import KMKKeyboard
-from kmk.extensions.rgb import RGB, AnimationModes
 from kmk.keys import KC
+from kmk.extensions.rgb import RGB, AnimationModes
+from kmk.extensions.media_keys import MediaKeys
+
 
 keyboard = KMKKeyboard(encoder=True)    # assume encoder installed
+keyboard.extensions.append(MediaKeys())
 
 XXXXX = KC.NO
 
 keyboard.keymap = [
     [
-        XXXXX,  KC.ESC, KC.N1,  KC.N2,  KC.N3,  KC.N4,  KC.N5,  KC.N6,  KC.N7,  KC.N8,  KC.N9,  KC.N0,  KC.MINS,KC.EQL, KC.BKSP,KC.HOME,
-        KC.ENT, KC.TAB, KC.Q,   KC.W,   KC.E,   KC.R,   KC.T,   KC.Y,   KC.U,   KC.I,   KC.O,   KC.P,   KC.LBRC,KC.RBRC,KC.BSLS,KC.DEL,
-        XXXXX,  KC.CAPS,KC.A,   KC.S,   KC.D,   KC.F,   KC.G,   KC.H,   KC.J,   KC.K,   KC.L,   KC.SCLN,KC.QUOT,KC.ENT, KC.ENT, KC.PGUP,
-        XXXXX,  KC.LSFT,KC.LSFT,KC.Z,   KC.X,   KC.C,   KC.V,   KC.B,   KC.N,   KC.M,   KC.COMM,KC.DOT, KC.SLSH,KC.RSFT,KC.UP,  KC.PGDN,
-        XXXXX,  KC.LCTL,KC.LCMD,KC.LALT,KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.RCMD,KC.RALT,KC.RCTL,KC.LEFT,KC.DOWN,KC.RGHT,
+        XXXXX,  KC.ESC, KC.N1,  KC.N2,  KC.N3,  KC.N4,  KC.N5,  KC.N6,  KC.N7,  KC.N8,  KC.N9,  KC.N0,  KC.MINS,KC.EQL, KC.BKSP,KC.DEL,
+        KC.MUTE,KC.TAB, KC.Q,   KC.W,   KC.E,   KC.R,   KC.T,   KC.Y,   KC.U,   KC.I,   KC.O,   KC.P,   KC.LBRC,KC.RBRC,KC.BSLS,KC.GRV,
+        KC.F1,  KC.CAPS,KC.A,   KC.S,   KC.D,   KC.F,   KC.G,   KC.H,   KC.J,   KC.K,   KC.L,   KC.SCLN,KC.QUOT,KC.ENT, KC.ENT, KC.PGUP,
+        KC.F2,  KC.LSFT,KC.Z,   KC.X,   KC.C,   KC.V,   KC.B,   KC.N,   KC.M,   KC.COMM,KC.DOT, KC.SLSH,KC.RSFT,XXXXX,  KC.UP,  KC.PGDN,
+        KC.F3,  KC.LCTL,KC.LCMD,KC.LALT,KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.RCMD,KC.RALT,KC.RCTL,KC.LEFT,XXXXX,  KC.DOWN,KC.RGHT,
     ]
+]
+
+# note that encoder button is configured in the keymap (KC.MUTE above) so set to XXXXX here
+keyboard.encoders.map = [
+    ( ( KC.VOLD, KC.VOLU, XXXXX),  ), # Layer 1, encoder 1
 ]
 
 rgb = RGB(
@@ -23,6 +31,7 @@ rgb = RGB(
     animation_speed=3,
     breathe_center=2,
 )
+
 keyboard.extensions.append(rgb)
 
 
