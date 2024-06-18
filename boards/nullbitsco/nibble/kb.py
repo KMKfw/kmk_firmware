fda61a09c925a6e310cfad1f682bdb59ecf40a3e
Patrick Surry <patrick.surry@gmail.com>
2024-06-18 18:27:07 -0400

    more lint

--- boards/nullbitsco/nibble/kb.py
+++ boards/nullbitsco/nibble/kb.py
@@ -2,12 +2,10 @@ import board
 import digitalio
 
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
-
 from kmk.modules.encoder import EncoderHandler
 from kmk.quickpin.pro_micro.bitc_promicro import pinout as pins
 from kmk.scanners import DiodeOrientation
 from kmk.scanners.digitalio import MatrixScanner
-
 
 # bitc pro pinout: https://nullbits.co/static/img/bitc_pro_pinout.png
 # nibble/tidbit pinout: https://github.com/nullbitsco/docs/blob/main/nibble/build_guide_img/mcu_pinouts.png
@@ -15,12 +13,13 @@ from kmk.scanners.digitalio import MatrixScanner
 # row connects to anode end, col connects to cathode end
 
 # also defined: board.LED_RED, board.LED_GREEN, and board.LED_BLUE == board.LED
-row_pins = (pins[15], pins[14], pins[13], pins[12], pins[6])    # GPIO 22,20,23,21,4
-col_mux_pins = (pins[19], pins[18], pins[17], pins[16])         # GPIO 29..26
-encoder_pins = (pins[10], pins[11], None)                       # GPIO 8,9, button in key matrix
-pixel_pin = pins[9]                                             # GPIO 7
+row_pins = (pins[15], pins[14], pins[13], pins[12], pins[6])  # GPIO 22,20,23,21,4
+col_mux_pins = (pins[19], pins[18], pins[17], pins[16])  # GPIO 29..26
+encoder_pins = (pins[10], pins[11], None)  # GPIO 8,9, button in key matrix
+pixel_pin = pins[9]  # GPIO 7
 # LED R, G, B pins: GPIO 6, 5, 3
 # extension pins GPIO 11, 12, 13, 14
+
 
 class KMKKeyboard(_KMKKeyboard):
     '''
@@ -32,7 +31,7 @@ class KMKKeyboard(_KMKKeyboard):
     '''
 
     pixel_pin = pixel_pin
-    i2c = board.I2C  #TODO ??
+    i2c = board.I2C  # TODO ??
 
     def __init__(self, encoder=False):
         super().__init__()
@@ -40,7 +39,7 @@ class KMKKeyboard(_KMKKeyboard):
         self.matrix = MatrixScanner(
             col_mux_pins,
             row_pins,
-            diode_orientation=DiodeOrientation.ROW2COL, # row is anode, col is cathode
+            diode_orientation=DiodeOrientation.ROW2COL,  # row is anode, col is cathode
             pull=digitalio.Pull.UP,
             multiplexed=True,
         )
--- boards/nullbitsco/nibble/main.py
+++ boards/nullbitsco/nibble/main.py
@@ -1,27 +1,102 @@
 from kb import KMKKeyboard
-from kmk.keys import KC
-from kmk.extensions.rgb import RGB, AnimationModes
+
 from kmk.extensions.media_keys import MediaKeys
+from kmk.extensions.rgb import RGB, AnimationModes
+from kmk.keys import KC
 
-
-keyboard = KMKKeyboard(encoder=True)    # assume encoder installed
+keyboard = KMKKeyboard(encoder=True)  # assume encoder installed
 keyboard.extensions.append(MediaKeys())
 
 XXXXX = KC.NO
 
 keyboard.keymap = [
     [
-        XXXXX,  KC.ESC, KC.N1,  KC.N2,  KC.N3,  KC.N4,  KC.N5,  KC.N6,  KC.N7,  KC.N8,  KC.N9,  KC.N0,  KC.MINS,KC.EQL, KC.BKSP,KC.DEL,
-        KC.MUTE,KC.TAB, KC.Q,   KC.W,   KC.E,   KC.R,   KC.T,   KC.Y,   KC.U,   KC.I,   KC.O,   KC.P,   KC.LBRC,KC.RBRC,KC.BSLS,KC.GRV,
-        KC.F1,  KC.CAPS,KC.A,   KC.S,   KC.D,   KC.F,   KC.G,   KC.H,   KC.J,   KC.K,   KC.L,   KC.SCLN,KC.QUOT,KC.ENT, KC.ENT, KC.PGUP,
-        KC.F2,  KC.LSFT,KC.Z,   KC.X,   KC.C,   KC.V,   KC.B,   KC.N,   KC.M,   KC.COMM,KC.DOT, KC.SLSH,KC.RSFT,XXXXX,  KC.UP,  KC.PGDN,
-        KC.F3,  KC.LCTL,KC.LCMD,KC.LALT,KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.RCMD,KC.RALT,KC.RCTL,KC.LEFT,XXXXX,  KC.DOWN,KC.RGHT,
+        XXXXX,
+        KC.ESC,
+        KC.N1,
+        KC.N2,
+        KC.N3,
+        KC.N4,
+        KC.N5,
+        KC.N6,
+        KC.N7,
+        KC.N8,
+        KC.N9,
+        KC.N0,
+        KC.MINS,
+        KC.EQL,
+        KC.BKSP,
+        KC.DEL,
+        KC.MUTE,
+        KC.TAB,
+        KC.Q,
+        KC.W,
+        KC.E,
+        KC.R,
+        KC.T,
+        KC.Y,
+        KC.U,
+        KC.I,
+        KC.O,
+        KC.P,
+        KC.LBRC,
+        KC.RBRC,
+        KC.BSLS,
+        KC.GRV,
+        KC.F1,
+        KC.CAPS,
+        KC.A,
+        KC.S,
+        KC.D,
+        KC.F,
+        KC.G,
+        KC.H,
+        KC.J,
+        KC.K,
+        KC.L,
+        KC.SCLN,
+        KC.QUOT,
+        KC.ENT,
+        KC.ENT,
+        KC.PGUP,
+        KC.F2,
+        KC.LSFT,
+        KC.Z,
+        KC.X,
+        KC.C,
+        KC.V,
+        KC.B,
+        KC.N,
+        KC.M,
+        KC.COMM,
+        KC.DOT,
+        KC.SLSH,
+        KC.RSFT,
+        XXXXX,
+        KC.UP,
+        KC.PGDN,
+        KC.F3,
+        KC.LCTL,
+        KC.LCMD,
+        KC.LALT,
+        KC.SPC,
+        KC.SPC,
+        KC.SPC,
+        KC.SPC,
+        KC.SPC,
+        KC.RCMD,
+        KC.RALT,
+        KC.RCTL,
+        KC.LEFT,
+        XXXXX,
+        KC.DOWN,
+        KC.RGHT,
     ]
 ]
 
 # note that encoder button is configured in the keymap (KC.MUTE above) so set to XXXXX here
 keyboard.encoders.map = [
-    ( ( KC.VOLD, KC.VOLU, XXXXX),  ), # Layer 1, encoder 1
+    ((KC.VOLD, KC.VOLU, XXXXX),),  # Layer 1, encoder 1
 ]
 
 rgb = RGB(
@@ -39,7 +114,7 @@ def set_backlight(hsv):
     rgb.hue, rgb.sat, rgb.val = hsv
 
 
-set_backlight((180,255,50))
+set_backlight((180, 255, 50))
 
 if __name__ == '__main__':
     keyboard.go()
