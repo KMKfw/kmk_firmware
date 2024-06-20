bfba224539e43b105ee824b7552b27dfd96a0e5f
Patrick Surry <patrick.surry@gmail.com>
2024-06-20 17:37:42 -0400

    flake8

--- boards/nullbitsco/nibble/main.py
+++ boards/nullbitsco/nibble/main.py
@@ -12,11 +12,11 @@ XXXXX = KC.NO
 # fmt: off
 keyboard.keymap = [
     [
-        XXXXX,  KC.ESC, KC.N1,  KC.N2,  KC.N3,  KC.N4,  KC.N5,  KC.N6,  KC.N7,  KC.N8,  KC.N9,  KC.N0,  KC.MINS,KC.EQL, KC.BKSP,KC.DEL,
-        KC.MUTE,KC.TAB, KC.Q,   KC.W,   KC.E,   KC.R,   KC.T,   KC.Y,   KC.U,   KC.I,   KC.O,   KC.P,   KC.LBRC,KC.RBRC,KC.BSLS,KC.GRV,
-        KC.F1,  KC.CAPS,KC.A,   KC.S,   KC.D,   KC.F,   KC.G,   KC.H,   KC.J,   KC.K,   KC.L,   KC.SCLN,KC.QUOT,KC.ENT, KC.ENT, KC.PGUP,
-        KC.F2,  KC.LSFT,KC.Z,   KC.X,   KC.C,   KC.V,   KC.B,   KC.N,   KC.M,   KC.COMM,KC.DOT, KC.SLSH,KC.RSFT,XXXXX,  KC.UP,  KC.PGDN,
-        KC.F3,  KC.LCTL,KC.LCMD,KC.LALT,KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.RCMD,KC.RALT,KC.RCTL,KC.LEFT,XXXXX,  KC.DOWN,KC.RGHT,
+        XXXXX,  KC.ESC, KC.N1,  KC.N2,  KC.N3,  KC.N4,  KC.N5,  KC.N6,  KC.N7,  KC.N8,  KC.N9,  KC.N0,  KC.MINS,KC.EQL, KC.BKSP,KC.DEL,     # noqa: E231
+        KC.MUTE,KC.TAB, KC.Q,   KC.W,   KC.E,   KC.R,   KC.T,   KC.Y,   KC.U,   KC.I,   KC.O,   KC.P,   KC.LBRC,KC.RBRC,KC.BSLS,KC.GRV,     # noqa: E231
+        KC.F1,  KC.CAPS,KC.A,   KC.S,   KC.D,   KC.F,   KC.G,   KC.H,   KC.J,   KC.K,   KC.L,   KC.SCLN,KC.QUOT,KC.ENT, KC.ENT, KC.PGUP,    # noqa: E231
+        KC.F2,  KC.LSFT,KC.Z,   KC.X,   KC.C,   KC.V,   KC.B,   KC.N,   KC.M,   KC.COMM,KC.DOT, KC.SLSH,KC.RSFT,XXXXX,  KC.UP,  KC.PGDN,    # noqa: E231
+        KC.F3,  KC.LCTL,KC.LCMD,KC.LALT,KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.SPC, KC.RCMD,KC.RALT,KC.RCTL,KC.LEFT,XXXXX,  KC.DOWN,KC.RGHT,    # noqa: E231
     ]
 ]
 # fmt: on
