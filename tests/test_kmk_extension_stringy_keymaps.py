e51fbf767b4668a92f7f83e982014e2e2e44aac9
xs5871 <xs5871@qn-4.net>
2024-07-26 20:57:40 +0000

    Fix unit test for string_keymaps

--- tests/test_kmk_extension_stringy_keymaps.py
+++ tests/test_kmk_extension_stringy_keymaps.py
@@ -8,14 +8,14 @@ from tests.keyboard_test import KeyboardTest
 class Test_extension_stringy_keymaps(unittest.TestCase):
     def test_basic_kmk_keyboard_replace_string_primary_name(self):
         keyboard = KeyboardTest(
-            [], [['1', '2', '3', '4']], extensions={StringyKeymaps()}
+            [], [['1', '2', '3', '4']], extensions=[StringyKeymaps()]
         )
 
         keyboard.test('Simple key press', [(0, True), (0, False)], [{KC.N1}, {}])
 
     def test_basic_kmk_keyboard_replace_string_secondary_name(self):
         keyboard = KeyboardTest(
-            [], [['N1', 'N2', 'N3', 'N4']], extensions={StringyKeymaps()}
+            [], [['N1', 'N2', 'N3', 'N4']], extensions=[StringyKeymaps()]
         )
 
         keyboard.test('Simple key press', [(0, True), (0, False)], [{KC.N1}, {}])
