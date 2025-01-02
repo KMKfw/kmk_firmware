3bfec6abdc25e905a34e27cf57c5c7027c56a2cf
Piman13 <70300512+piman13@users.noreply.github.com>
2025-01-01 21:51:06 -0500

    Rework of analogin to to function like encoder
    AnalogInputs turned into AnalogHandler and takes in all the stuff needed for setting up and takes muxed into inputs once added other wise it can use direct pin (simalar to i2c in encoder)
    looks like it should work but currenly running into "memory allocation failed" errors. no idea how to reduce it

--- kmk/modules/analogin.py
+++ kmk/modules/analogin.py
@@ -4,6 +4,16 @@ from kmk.utils import Debug
 
 debug = Debug(__name__)
 
+DEFAULT_FILTER = lambda input: input >> 8
+
+
+#thinking notes for mux
+#muxing uses dio pins for ch select. while if you have multiple muxers they should all use the same ch select
+#some might not follow this. the option should be provided to map seperate pins for each adc
+#while there should not be a max to the number of channels there might be a true scanning max before delay holds
+#other systems to fault (not sure if this could be threaded or if kmk uses threding to help but if you need
+#32 switches on one adc you have a big board or... a big midi pad...)
+
 
 def noop(*args):
     pass
@@ -42,80 +52,159 @@ class AnalogKey(AnalogEvent):
 
 
 class AnalogInput:
-    def __init__(self, input, filter=lambda input: input.value >> 8, sensitivity=1):
+    def __init__(self, input, filter=DEFAULT_FILTER, invert=False, sensitivity=1):
         self.input = input
         self.value = 0
         self.delta = 0
+        self.invert = invert
         self.sensitivity = sensitivity
-        self.filter = filter
 
-    def update(self):
-        '''
-        Read a new value from an analogio compatible input, apply
-        transformation, then return either the new value if it changed or `None`
-        otherwise.
-        '''
+        ## invert the filter by changing the outputed value to a negitive then adding back to positive
+        ## and getting max val from the filter so even very custom filters can work
+        if self.invert == True:
+            print("here")
+            filtermax = filter(65535) + 1 #eval filter max to not run every time
+            print("not")
+            self.filter = lambda input:((~filter) + filtermax)
+        else:
+            self.filter = filter
+
+    def update_state(self, idx):
+        print("updating")
         value = self.filter(self.input)
         self.delta = value - self.value
         if self.delta not in range(-self.sensitivity, self.sensitivity):
             self.value = value
-            return value
+            self.on_move_do(idx)
+            
 
 
-class AnalogInputs(Module):
-    def __init__(self, inputs, evtmap):
-        self._active = {}
-        self.inputs = inputs
-        self.evtmap = evtmap
 
+class MuxedAnlogInput:
+     def __init__(self,idx):
+         print("tmp")
+
+## 
+##def update(self):
+##        '''
+##        Read a new value from an analogio compatible input, apply
+##        transformation, then return either the new value if it changed or `None`
+##        otherwise.
+##        '''
+##
+##        
+
+
+##        for idx, input in enumerate(self.inputs):
+##            value = input.update()
+##
+##            # No change in value: stop or pass
+##            if value is None:
+##                if input in self._active:
+##                    if debug.enabled:
+##                        debug('on_stop', input, self._active[idx])
+##                    self._active[idx].on_stop(input, keyboard)
+##                    del self._active[idx]
+##                continue
+##
+##            # Resolve event handler
+##            if input in self._active:
+##                key = self._active[idx]
+##            else:
+##                key = None
+##                for layer in keyboard.active_layers:
+##                    try:
+##                        key = self.evtmap[layer][idx]
+##                    except IndexError:
+##                        if debug.enabled:
+##                            debug('evtmap IndexError: idx=', idx, ' layer=', layer)
+##                    if key and key != KC.TRNS:
+##                        break
+##
+##            if key == KC.NO:
+##                continue
+##
+##            # Forward change to event handler
+##            try:
+##                self._active[idx] = key
+##                if debug.enabled:
+##                    debug('on_change', input, key, value)
+##                key.on_change(input, keyboard)
+##            except Exception as e:
+##                if debug.enabled:
+##                    debug(type(e), ': ', e, ' in ', key.on_change)
+##
+
+
+
+class AnalogHandler(Module):
+    def __init__(
+         self,
+         apins,
+         mpins = [],
+         disabledmuxs = [],
+         evtmap,
+         invertmap = [False],
+         filtermap = [DEFAULT_FILTER],
+         sensitivitymap =[1]
+         ):
+        
+        self.analogs = []
+        self.apins = apins #Take in chosen analog pins
+        self.mpins = mpins #pins for muxing interface
+        self.disabledmuxs = emuxs #2d array containing enabled mux ports per layer
+        self.evtmap = evtmap  #event mapping for inputs
+        self.filtermap = filtermap # filter array: all same if only first slot filled
+        self.invertmap = invertmap # simple bool array for inverting either all if only first slot filled
+        self.sensitivitymap = sensitivitymap
+        
     def on_runtime_enable(self, keyboard):
         return
 
     def on_runtime_disable(self, keyboard):
         return
 
+    def on_move_do(self, keyboard, idx):
+        print(idx)
+        print(self.value)
+    
     def during_bootup(self, keyboard):
+        if self.apins and self.evtmap:
+            if len(self.mpins) > 0:
+                #pass to mux handler
+                print("not implemented yet")
+            else:
+                #direct pin mode
+                for idx, pin in enumerate(self.apins):
+                    #allow for filter and invert to be either fill all or per io
+                    if len(self.filtermap) > 1:
+                        filter = self.filtermap[idx]
+                    else:
+                        filter = self.filtermap[0]
+
+                    if len(self.invertmap) > 1:
+                        invert = self.invertmap[idx]
+                    else:
+                        invert = self.invertmap[0]
+
+                    if len(self.sensitivitymap) > 1:
+                        sensitivity = self.sensitivitymap[idx]
+                    else:
+                        sensitivity = self.sensitivitymap[0]
+
+                    self.analogs.append
+                    (
+                        AnalogInput(AnalogIn(pin),filter, invert, sensitivity)
+                    )
         return
 
     def before_matrix_scan(self, keyboard):
-        for idx, input in enumerate(self.inputs):
-            value = input.update()
+        for idx, analog in enumerate self.analogs:
+            analog.update_state(idx)
+        return keyboard
+        
+        
 
-            # No change in value: stop or pass
-            if value is None:
-                if input in self._active:
-                    if debug.enabled:
-                        debug('on_stop', input, self._active[idx])
-                    self._active[idx].on_stop(input, keyboard)
-                    del self._active[idx]
-                continue
-
-            # Resolve event handler
-            if input in self._active:
-                key = self._active[idx]
-            else:
-                key = None
-                for layer in keyboard.active_layers:
-                    try:
-                        key = self.evtmap[layer][idx]
-                    except IndexError:
-                        if debug.enabled:
-                            debug('evtmap IndexError: idx=', idx, ' layer=', layer)
-                    if key and key != KC.TRNS:
-                        break
-
-            if key == KC.NO:
-                continue
-
-            # Forward change to event handler
-            try:
-                self._active[idx] = key
-                if debug.enabled:
-                    debug('on_change', input, key, value)
-                key.on_change(input, keyboard)
-            except Exception as e:
-                if debug.enabled:
-                    debug(type(e), ': ', e, ' in ', key.on_change)
 
     def after_matrix_scan(self, keyboard):
         return
