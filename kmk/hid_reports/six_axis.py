bdcae284b43899a781ee4bb839f3255da107293f
Glodigit <kdengelin@hotmail.com>
2025-02-19 23:20:18 +0000

    descriptor comments at col28

--- kmk/hid_reports/six_axis.py
+++ kmk/hid_reports/six_axis.py
@@ -43,43 +43,43 @@ SIX_AXIS = usb_hid.Device(
 
 # fmt:off
 report_descriptor = bytes(
-    (                       # (From usb_hid/Device.c)
-        0x05, 0x01,         # Usage Page (Generic Desktop Ctrls)
-        0x09, 0x06,         # Usage (Keyboard)
-        0xA1, 0x01,         # Collection (Application)
-        0x85, 0x04,         #   Report ID (4)
+    (                      # (From usb_hid/Device.c)
+        0x05, 0x01,        # Usage Page (Generic Desktop Ctrls)
+        0x09, 0x06,        # Usage (Keyboard)
+        0xA1, 0x01,        # Collection (Application)
+        0x85, 0x04,        #   Report ID (4)
 
-        0x05, 0x07,         #   Usage Page (Kbrd/Keypad)
-        0x19, 0xE0,         #   Usage Minimum (0xE0)
-        0x29, 0xE7,         #   Usage Maximum (0xE7)
-        0x15, 0x00,         #   Logical Minimum (0)
-        0x25, 0x01,         #   Logical Maximum (1)
-        0x75, 0x01,         #   Report Size (1)
-        0x95, 0x08,         #   Report Count (8)
-        0x81, 0x02,         #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-        0x95, 0x01,         #   Report Count (1)
-        0x75, 0x08,         #   Report Size (8)
-        0x81, 0x01,         #   Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-        0x95, 0x05,         #   Report Count (5)
-        0x75, 0x01,         #   Report Size (1)
+        0x05, 0x07,        #   Usage Page (Kbrd/Keypad)
+        0x19, 0xE0,        #   Usage Minimum (0xE0)
+        0x29, 0xE7,        #   Usage Maximum (0xE7)
+        0x15, 0x00,        #   Logical Minimum (0)
+        0x25, 0x01,        #   Logical Maximum (1)
+        0x75, 0x01,        #   Report Size (1)
+        0x95, 0x08,        #   Report Count (8)
+        0x81, 0x02,        #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+        0x95, 0x01,        #   Report Count (1)
+        0x75, 0x08,        #   Report Size (8)
+        0x81, 0x01,        #   Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+        0x95, 0x05,        #   Report Count (5)
+        0x75, 0x01,        #   Report Size (1)
 
-        0x05, 0x08,         #   Usage Page (LEDs)
-        0x19, 0x01,         #   Usage Minimum (Num Lock)
-        0x29, 0x05,         #   Usage Maximum (Kana)
-        0x91, 0x02,         #   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-        0x95, 0x01,         #   Report Count (1)
-        0x75, 0x03,         #   Report Size (3)
-        0x91, 0x01,         #   Output (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-        0x95, 0x06,         #   Report Count (6)
-        0x75, 0x08,         #   Report Size (8)
-        0x15, 0x00,         #   Logical Minimum (0)
-        0x26, 0xFF, 0x00,   #   Logical Maximum (255)
+        0x05, 0x08,        #   Usage Page (LEDs)
+        0x19, 0x01,        #   Usage Minimum (Num Lock)
+        0x29, 0x05,        #   Usage Maximum (Kana)
+        0x91, 0x02,        #   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+        0x95, 0x01,        #   Report Count (1)
+        0x75, 0x03,        #   Report Size (3)
+        0x91, 0x01,        #   Output (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+        0x95, 0x06,        #   Report Count (6)
+        0x75, 0x08,        #   Report Size (8)
+        0x15, 0x00,        #   Logical Minimum (0)
+        0x26, 0xFF, 0x00,  #   Logical Maximum (255)
 
-        0x05, 0x07,         #   Usage Page (Kbrd/Keypad)
-        0x19, 0x00,         #   Usage Minimum (0x00)
-        0x2A, 0xFF, 0x00,   #   Usage Maximum (0xFF)
-        0x81, 0x00,         #   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-        0xC0,               # End Collection
+        0x05, 0x07,        #   Usage Page (Kbrd/Keypad)
+        0x19, 0x00,        #   Usage Minimum (0x00)
+        0x2A, 0xFF, 0x00,  #   Usage Maximum (0xFF)
+        0x81, 0x00,        #   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+        0xC0,              # End Collection
     )
 )
 # fmt:on
@@ -97,39 +97,39 @@ KEYBOARD = usb_hid.Device(
 # fmt:off
 report_descriptor = bytes(
     (
-        0x05, 0x01,  # Usage Page (Generic Desktop Ctrls),
-        0x09, 0x06,  # Usage (Keyboard),
-        0xA1, 0x01,  # Collection (Application),
-        0x85, 0x04,  #   Report ID (4)
-        # modifiers
-        0x05, 0x07,  #   Usage Page (Key Codes),
-        0x19, 0xE0,  #   Usage Minimum (224),
-        0x29, 0xE7,  #   Usage Maximum (231),
-        0x15, 0x00,  #   Logical Minimum (0),
-        0x25, 0x01,  #   Logical Maximum (1),
-        0x75, 0x01,  #   Report Size (1),
-        0x95, 0x08,  #   Report Count (8),
-        0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+        0x05, 0x01,        # Usage Page (Generic Desktop Ctrls),
+        0x09, 0x06,        # Usage (Keyboard),
+        0xA1, 0x01,        # Collection (Application),
+        0x85, 0x04,        #   Report ID (4)
+        # Modifiers
+        0x05, 0x07,        #   Usage Page (Key Codes),
+        0x19, 0xE0,        #   Usage Minimum (224),
+        0x29, 0xE7,        #   Usage Maximum (231),
+        0x15, 0x00,        #   Logical Minimum (0),
+        0x25, 0x01,        #   Logical Maximum (1),
+        0x75, 0x01,        #   Report Size (1),
+        0x95, 0x08,        #   Report Count (8),
+        0x81, 0x02,        #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
         # LEDs
-        0x05, 0x08,  #   Usage Page (LEDs),
-        0x19, 0x01,  #   Usage Minimum (1),
-        0x29, 0x05,  #   Usage Maximum (5),
-        0x95, 0x05,  #   Report Count (5),
-        0x75, 0x01,  #   Report Size (1),
-        0x91, 0x02,  #   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non- olatile)
-        0x95, 0x01,  #   Report Count (1),
-        0x75, 0x03,  #   Report Size (3),
-        0x91, 0x01,  #   Output (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,N n-volatile)
-        # keys
-        0x05, 0x07,  #   Usage Page (Kbrd/Keypad),
-        0x19, 0x00,  #   Usage Minimum (0),
-        0x29, 0x77,  #   Usage Maximum (119),
-        0x15, 0x00,  #   Logical Minimum (0),
-        0x25, 0x01,  #   Logical Maximum(1),
-        0x95, 0x78,  #   Report Count (120),
-        0x75, 0x01,  #   Report Size (1),
-        0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-        0xC0,        # End Collection
+        0x05, 0x08,        #   Usage Page (LEDs),
+        0x19, 0x01,        #   Usage Minimum (1),
+        0x29, 0x05,        #   Usage Maximum (5),
+        0x95, 0x05,        #   Report Count (5),
+        0x75, 0x01,        #   Report Size (1),
+        0x91, 0x02,        #   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non- olatile)
+        0x95, 0x01,        #   Report Count (1),
+        0x75, 0x03,        #   Report Size (3),
+        0x91, 0x01,        #   Output (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,N n-volatile)
+        # Keys
+        0x05, 0x07,        #   Usage Page (Kbrd/Keypad),
+        0x19, 0x00,        #   Usage Minimum (0),
+        0x29, 0x77,        #   Usage Maximum (119),
+        0x15, 0x00,        #   Logical Minimum (0),
+        0x25, 0x01,        #   Logical Maximum(1),
+        0x95, 0x78,        #   Report Count (120),
+        0x75, 0x01,        #   Report Size (1),
+        0x81, 0x02,        #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+        0xC0,              # End Collection
     )
 )
 # fmt:on
