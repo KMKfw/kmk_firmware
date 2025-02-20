062e163b134427ad3ab0691b7c3233207abf9d23
Glodigit <kdengelin@hotmail.com>
2025-02-20 02:03:08 +0000

    remove class AxisReport and correct SixAxisDeviceReport.buffer bytes

--- kmk/hid.py
+++ kmk/hid.py
@@ -144,24 +144,7 @@ class ConsumerControlReport(Report):
         return {ConsumerKey: self.add_cc}
 
 
-class AxisReport(Report):
-    def __init__(self, size, clamp=127):
-        self.buffer = bytearray(size)
-        self.clamp = clamp
-        self.pending = False
-
-    def move_axis(self, axis):
-        delta = clamp(axis.delta, -self.clamp, self.clamp)
-        axis.delta -= delta
-        try:
-            self.buffer[axis.code + 1] = 0xFF & delta
-            self.pending = True
-        except IndexError:
-            if debug.enabled:
-                debug(axis, ' not supported')
-
-
-class PointingDeviceReport(AxisReport):
+class PointingDeviceReport(Report):
     def __init__(self, size=_REPORT_SIZE_MOUSE):
         super().__init__(size)
 
@@ -173,6 +156,16 @@ class PointingDeviceReport(AxisReport):
         self.buffer[0] &= ~key.code
         self.pending = True
 
+    def move_axis(self, axis):
+        delta = clamp(axis.delta, -127, 127)
+        axis.delta -= delta
+        try:
+            self.buffer[axis.code + 1] = 0xFF & delta
+            self.pending = True
+        except IndexError:
+            if debug.enabled:
+                debug(axis, ' not supported')
+
     def get_action_map(self):
         return {Axis: self.move_axis, MouseKey: self.add_button}
 
@@ -182,9 +175,21 @@ class HSPointingDeviceReport(PointingDeviceReport):
         super().__init__(_REPORT_SIZE_MOUSE_HSCROLL)
 
 
-class SixAxisDeviceReport(AxisReport):
-    def __init__(self, size=_REPORT_SIZE_SIXAXIS, clamp=500):
+class SixAxisDeviceReport(Report):
+    def __init__(self, size=_REPORT_SIZE_SIXAXIS):
         super().__init__(size, clamp)
+
+    def move_axis(self, axis):
+        delta = clamp(axis.delta, -500, 500)
+        axis.delta -= delta
+        index = 2 * axis.code
+        try:
+            self.buffer[index - 1] = 0xFF & delta
+            self.buffer[index] = 0x03 & (delta >> 8)
+            self.pending = True
+        except IndexError:
+            if debug.enabled:
+                debug(axis, ' not supported')
 
     def get_action_map(self):
         return {SixAxis: self.move_axis}
