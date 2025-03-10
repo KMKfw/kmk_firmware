e121144f59396dd2ec100f461a80e9ffc65210c1
RasmusB <RasmusB@users.noreply.github.com>
2025-03-10 12:34:16 +0100

    Fix formattning

--- boards/psion/code.py
+++ boards/psion/code.py
@@ -23,7 +23,7 @@ keyboard.col_pins = (
     board.GP4,
     board.GP3,
     board.GP2,
-    board.GP0
+    board.GP0,
     )
 
 keyboard.row_pins = (
@@ -40,7 +40,7 @@ keyboard.row_pins = (
 # Choose your layout here
 # ==================================
 
-import layout_swe as keyboard_layout # noqa: E402
+import layout_swe as keyboard_layout  # noqa: E402
 
 # ==================================
 
